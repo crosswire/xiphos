3b54120e19b849dc9671c781fcc3553a77a2970d
(no author) <(no author)@da23c209-522a-4b7c-92e9-24af42be4764>
2000-05-11 18:20:24 +0000

    This commit was manufactured by cvs2svn to create branch 'vendor'.
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/branches/vendor@6 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ gnomesword/gnomesword/filestuff.h
@@ -0,0 +1,50 @@
+/***************************************************************************
+                          filestuff.h  -  description
+                             -------------------
+    begin                : Mon May 8 2000
+    copyright            : (C) 2000 by Terry Biggs
+    email                :  tbiggs@infinet.com
+ ***************************************************************************/
+
+ /*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+void
+saveFile 					(gchar *filename);
+
+void
+loadFile 					(GtkWidget *filesel);
+
+void
+setDiretory					(void);
+
+void
+loadbookmarkarray		(void);
+
+void
+loadoptionarray			(void);
+
+void
+savebookmarks			(void);
+
+void
+editbookmarksSave	   (GtkWidget *editdlg);
+ 
+void
+saveoptions				 (void) ;
+
+void
+createFiles					(void);
