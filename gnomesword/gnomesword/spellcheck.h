aa000d34596f0551129795d0b9f8ff10e4684382
Terry Biggs <tbiggs@users.sf.net>
2000-05-19 02:04:38 +0000

    header file for spell check support - new file
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@12 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ gnomesword/gnomesword/spellcheck.h
@@ -0,0 +1,23 @@
+/***************************************************************************
+                          spellcheck.h  -  description
+                             -------------------
+    begin                : Tue May 16 2000
+    copyright            : (C) 2000 by Terry Biggs
+    email                : tbiggs@infinet.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+//#ifdef USE_ASPELL
+#include <gnome.h>
+
+GtkWidget*
+spellcheck(gint itext/*textwindow to check*/);
+
+//#endif /* USE_ASPELL */
