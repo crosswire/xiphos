7aa6ac85a2bf933d307b62955b6fe3610740d622
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2000-06-15 01:40:50 +0000

    2000-06-14 settings structure
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@20 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/gnomesword/filestuff.h
+++ gnomesword/gnomesword/filestuff.h
@@ -21,6 +21,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+#include "GnomeSword.h"
 
 void
 saveFile 					(gchar *filename);
@@ -48,3 +49,12 @@ saveoptions				 (void) ;
 
 void
 createFiles					(void);
+
+void
+writesettings				(SETTINGS settings);
+
+SETTINGS
+readsettings				(void);
+
+SETTINGS
+createsettings			(void);
