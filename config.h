9fa1bd2c42fed2e11e32f28408cda82b93ea9be5
Terry Biggs <tbiggs@users.sf.net>
2000-07-13 23:43:24 +0000

    added notes formatting
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword@32 da23c209-522a-4b7c-92e9-24af42be4764

--- config.h
+++ config.h
@@ -24,7 +24,7 @@
 #define PACKAGE "gnomesword"
 
 /* Version number of package */
-#define VERSION "0.1.8f"
+#define VERSION "0.1.8g"
 
 /* Define if you want to use aspell spell checking */
 #define USE_ASPELL 1
