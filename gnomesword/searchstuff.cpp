3fbbfb7b6d3b300cc59c2d8a33c3939e0855c078
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2000-09-05 20:32:51 +0000

    more code style changes
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword@56 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/searchstuff.cpp
+++ gnomesword/searchstuff.cpp
@@ -39,17 +39,17 @@
 
 SearchWindow 			*searchWindow;
 extern SWModule 		*curMod,
-						*curcomMod,
-						*percomMod;
+				*curcomMod,
+				*percomMod;
 
 SearchWindow::SearchWindow()
 {
 	searchType_group 	= NULL;
-	_3_group 			= NULL;
+	_3_group 		= NULL;
 	searchMgr       	= new SWMgr();	
-	searchMod			= NULL;
-	plaintohtml			= new PLAINHTML(); //-- sword renderfilter Plain to html
-	gbftohtml			= new GBFHTML(); //-- sword renderfilter gbf to html
+	searchMod		= NULL;
+	plaintohtml		= new PLAINHTML(); //-- sword renderfilter Plain to html
+	gbftohtml		= new GBFHTML(); //-- sword renderfilter gbf to html
 	PLAINsearchDisplay 	=0; // set in create
 	HTMLsearchDisplay 	=0; // set in create
 	RWPsearchDisplay 	=0; // set in create	
@@ -88,19 +88,17 @@ SearchWindow::initsearchWindow(GtkWidget *searchDlg) //-- init search dialog
 	ModMap::iterator it;  //-- sword manager iterator
 	SectionMap::iterator sit; //-- iteratior
 	ConfigEntMap::iterator cit; //-- iteratior
-	gchar *sourceformat;
-	
-	
+	gchar *sourceformat;	
 
 	gtk_text_set_word_wrap(GTK_TEXT (lookup_widget(searchDlg,"txtSearch")) , TRUE ); //-- set text window to word wrap
 	PLAINsearchDisplay = new GTKEntryDisp(lookup_widget(searchDlg,"txtSearch")); //-- set sword display for gbf
 	HTMLsearchDisplay = new HTMLentryDisp(lookup_widget(searchDlg,"txtSearch")); //-- set sword display for html
 	RWPsearchDisplay = new GTKRWPDisp(lookup_widget(searchDlg,"txtSearch")); //-- set sword display for html
     //--------------------------------------------------------------------------------------- searchmodule	
-	for (it = searchMgr->Modules.begin(); it != searchMgr->Modules.end(); it++) //-- iterator through modules
+	for (it = searchMgr->Modules.begin(); it != searchMgr->Modules.end(); it++) /* iterator through modules */
 	{
 		searchMod  = (*it).second;  //-- set searchMod
-		sit = searchMgr->config->Sections.find((*it).second->Name()); //-- check to see if we need render filters
+		sit = searchMgr->config->Sections.find((*it).second->Name()); /* check to see if we need render filters */
 	    if (sit !=searchMgr->config->Sections.end())
 	    {
 	    	cit = (*sit).second.find("SourceType");
@@ -109,22 +107,22 @@ SearchWindow::initsearchWindow(GtkWidget *searchDlg) //-- init search dialog
 			else
 				sourceformat = "Plain";
 		}
-		if (!strcmp(sourceformat, "GBF")) //-- we need gbf to html filter
+		if (!strcmp(sourceformat, "GBF")) /* we need gbf to html filter */
 		{
 			searchMod->AddRenderFilter(gbftohtml);
 			searchMod->Disp(HTMLsearchDisplay);
 				
 		}
-		else if(!strcmp(sourceformat,"ThML")) //-- we need ThML to html filter
+		else if(!strcmp(sourceformat,"ThML")) /* we need ThML to html filter */
 		{
-			//curcomMod->AddRenderFilter(thmltogbf);
+			//curcomMod->AddRenderFilter(thmltohtml);
 			searchMod->Disp(HTMLsearchDisplay);
 		}
-		else if((*searchMgr->config->Sections[(*it).second->Name()].find("ModDrv")).second == "RawFiles") //-- if driver is RawFiles
+		else if((*searchMgr->config->Sections[(*it).second->Name()].find("ModDrv")).second == "RawFiles") /* if driver is RawFiles */
 		{   				 	
 			searchMod->Disp(HTMLsearchDisplay); //-- no filter needed
 		}				
-		else if(!strcmp(searchMod->Name(),"RWP"))  //-- the rwp to html filter needs work
+		else if(!strcmp(searchMod->Name(),"RWP"))  /* the rwp to html filter needs work */
 			searchMod->Disp(RWPsearchDisplay);     //-- so we will use our own
 		else
 		{				
@@ -140,22 +138,24 @@ void
 SearchWindow::searchSWORD(GtkWidget *searchFrm)  //-- search Bible text or commentaries
 {
 	ModMap::iterator it;
-	GtkWidget	*searchText,   //-- what we want to find -text entry
-					 	*lbSearchHits,  //-- label for showing how many verses we found
-					 	*resultList,    //-- list of verses found
-					 	*regexSearch,   //-- do we want to use regular expression search - radio button
-					 	*phraseSearch,  //-- do we want to use phrase seach - radio button
-					 	*caseSensitive, //-- do we want search to be case sensitive - check box
-					 	*comToggle,     //-- do we want to search current commentary - check box
-					 	*bounds,        //-- do we want to use bounds for our search - check box
-					 	*lastsearch,    //-- use verses from last search for bounds of this search
-					 	*textWindow,    //-- text widget to display verses
-					 	*percomToggle;  //-- do we want to search personal commentary - check box
-	string 		srchText;       //-- string to search for - from entryText
-	gchar     *entryText,//-- pointer to text in searchText entry
-			      scount[5];  //-- string from gint count for label
-	gchar 		*resultText; //-- temp storage for verse found
-	gint count;              //-- number of hits
+	GtkWidget       *searchText,   //-- what we want to find -text entry
+			*lbSearchHits, //-- label for showing how many verses we found
+			*resultList,   //-- list of verses found
+			*regexSearch,  //-- do we want to use regular expression search - radio button
+			*phraseSearch, //-- do we want to use phrase seach - radio button
+			*caseSensitive,//-- do we want search to be case sensitive - check box
+			*comToggle,    //-- do we want to search current commentary - check box
+			*bounds,       //-- do we want to use bounds for our search - check box
+			*lowerbound,        //-------- lower bounds entry widget
+			*upperbound,        //-------- upper bounds entry widget
+			*lastsearch,   //-- use verses from last search for bounds of this search
+			*textWindow,   //-- text widget to display verses
+			*percomToggle; //-- do we want to search personal commentary - check box
+	//string 		srchText;      //-- string to search for - from entryText
+	gchar           *entryText,    //-- pointer to text in searchText entry
+			scount[5];     //-- string from gint count for label
+	gchar 		*resultText;   //-- temp storage for verse found
+	gint            count;         //-- number of hits
 
 	searchText = lookup_widget(searchFrm,"entry1"); //-- pointer to text entry
 	lbSearchHits = lookup_widget(searchFrm,"lbSearchHits");//-- pointer to count label
@@ -173,44 +173,43 @@ SearchWindow::searchSWORD(GtkWidget *searchFrm)  //-- search Bible text or comme
 	gtk_text_set_point(GTK_TEXT(textWindow), 0); //-- clear text window
 	gtk_text_forward_delete (GTK_TEXT (textWindow), gtk_text_get_length((GTK_TEXT(textWindow))));
 
-	if(GTK_TOGGLE_BUTTON(comToggle)->active)//-- if true search commentary
+	if(GTK_TOGGLE_BUTTON(comToggle)->active) /* if true search commentary */
 	{
-		it = searchMgr->Modules.find(curcomMod->Name()); //-- find commentary module
+		it = searchMgr->Modules.find(curcomMod->Name()); /* find commentary module */
 		if (it != searchMgr->Modules.end())
 		{
 			searchMod = (*it).second;  //-- set search module to current commentary module
 	 	}
 	} 		
-	else if(GTK_TOGGLE_BUTTON(percomToggle)->active) //-- if true search personal commentary
+	else if(GTK_TOGGLE_BUTTON(percomToggle)->active) /* if true search personal commentary */
 	{
-	    it = searchMgr->Modules.find(percomMod->Name()); //-- find personal commentary module
+	        it = searchMgr->Modules.find(percomMod->Name()); /* find personal commentary module */
 		if (it != searchMgr->Modules.end())
 		{
 			searchMod = (*it).second;  //-- set search module to current personalcommentary module
 	 	} 	
 	}
-	else    //-- if neither commertary nor personal check box checked
+	else    /* if neither commertary nor personal check box checked */
 	{
-	    it = searchMgr->Modules.find(curMod->Name()); //-- find personal commentary module
+	        it = searchMgr->Modules.find(curMod->Name()); //-- find personal commentary module
 		if (it != searchMgr->Modules.end())
 		{
 			searchMod = (*it).second;  //-- set search module to current personalcommentary module
 	 	} 	
 	}
-	if(GTK_TOGGLE_BUTTON(bounds)->active) //---------- check to see if we want to set bounds for search
-	{
-		GtkWidget   *lower, //-------- lower bounds entry widget
-					*upper; //-------- upper bounds entry widget
-		lower = lookup_widget(searchFrm,"entryLower"); //----------- get Lower bounds entry widget from search form
-		upper = lookup_widget(searchFrm,"entryUpper"); //----------- get Upper bounds entry widget from search form	
+	
+	if(GTK_TOGGLE_BUTTON(bounds)->active)  
+	{	
+		lowerbound = lookup_widget(searchFrm,"entryLower"); //----------- get Lower bounds entry widget from search form
+		upperbound = lookup_widget(searchFrm,"entryUpper"); //----------- get Upper bounds entry widget from search form	
 		searchScopeLowUp.ClearBounds(); //-------- clear old bounds
-		searchScopeLowUp.LowerBound(gtk_entry_get_text(GTK_ENTRY(lower))); //--read lower bounds entry and set lower bounds for search
-		searchScopeLowUp.UpperBound(gtk_entry_get_text(GTK_ENTRY(upper))); //--read upper bounds entry and set upper bounds for search
+		searchScopeLowUp.LowerBound(gtk_entry_get_text(GTK_ENTRY(lowerbound))); //--read lower bounds entry and set lower bounds for search
+		searchScopeLowUp.UpperBound(gtk_entry_get_text(GTK_ENTRY(upperbound))); //--read upper bounds entry and set upper bounds for search
 		currentScope = &searchScopeLowUp; //------------- set scope of search to use bounds	
 	}
-	else if(GTK_TOGGLE_BUTTON(lastsearch)->active) //---------- check to see if we want to use results of search last for this search
+	else if(GTK_TOGGLE_BUTTON(lastsearch)->active) /* check to see if we want to use results of search last for this search */
 	{
-		currentScope = &searchScopeList; //-- if we do = move searchlist into currentScope
+		currentScope = &searchScopeList; //-- if we do _ move searchlist into currentScope
 	}
 	else
 	{
@@ -219,19 +218,19 @@ SearchWindow::searchSWORD(GtkWidget *searchFrm)  //-- search Bible text or comme
 		currentScope = 0; //------------ clear scope
 	}
 
-	count = 0;    //-- set count to 0 - we have not found anything yet
+	count = 0;    /* we have not found anything yet */
 	gtk_clist_clear(GTK_CLIST(resultList));	//-- clear list widget for new results
 
 	entryText = gtk_entry_get_text(GTK_ENTRY(searchText)); //-- what to search for
-	srchText = entryText; //-- move from char* to string
+	//srchText = entryText; //-- move from char* to string
 	gtk_label_set_text( GTK_LABEL(lbSearchHits) ,"0" ); //-- set hits label to 0
-	if (searchMod)  //-- must have a good module - not null
+	if (searchMod) /* must have a good module - not null */
 	{
 		int searchType = GTK_TOGGLE_BUTTON(regexSearch)->active ? 0 : GTK_TOGGLE_BUTTON(phraseSearch)->active ? -1 : -2; //-- get search type
 		int searchParams = GTK_TOGGLE_BUTTON(caseSensitive)->active ? 0 : REG_ICASE; //-- get search params - case sensitive
 		gtk_clist_freeze(GTK_CLIST(resultList)); //-- keep list form scrolling until we are done
-		for (ListKey &searchResults = searchMod->Search(srchText.c_str(), searchType, //-- give search string to module to search
-									searchParams, currentScope); !searchResults.Error(); searchResults++)
+		//-- give search string to module to search
+		for (ListKey &searchResults = searchMod->Search(entryText, searchType, searchParams, currentScope); !searchResults.Error(); searchResults++)
 		{
 			resultText = g_strdup((const char *)searchResults);  //-- put verse key string of find into a string
 			gtk_clist_append(GTK_CLIST(resultList), &resultText); //-- store find in list
@@ -255,14 +254,15 @@ SearchWindow::resultsListSWORD(GtkWidget *searchFrm, gint row, gint column) //--
 	resultList = lookup_widget(searchFrm,"resultList"); //-- set pointer to resultList
 	contextToggle = lookup_widget(searchFrm,"cbContext"); //-- set pointer to context check box 	
 	gtk_clist_get_text(GTK_CLIST(resultList), row, column, &text); //-- get key text from resultlist
-	if(searchMod) //-- make sure module is not null
+	if(searchMod) /* make sure module is not null */
 	{
 		searchMod->SetKey(text); //-- set module to verse key text
 		searchMod->Display();    //-- show verse or commentary
 	}
-	if(GTK_TOGGLE_BUTTON(contextToggle)->active) //-- check state of context check box
-	{                        //-- if true
-		changeVerse(text);	//-- show selection in main window
+	if(GTK_TOGGLE_BUTTON(contextToggle)->active) /* check state of context check box */
+	{                        
+	        /* if true */
+		changeVerse(text);	/* show selection in main window */
 	}
 }
 
@@ -270,9 +270,6 @@ SearchWindow::resultsListSWORD(GtkWidget *searchFrm, gint row, gint column) //--
 GtkWidget*
 SearchWindow::create()
 {
-
-
-
   dlgSearch = gnome_dialog_new ("GnomeSword - Search", NULL);
   gtk_object_set_data (GTK_OBJECT (dlgSearch), "dlgSearch", dlgSearch);
   gtk_container_set_border_width (GTK_CONTAINER (dlgSearch), 4);
@@ -617,3 +614,4 @@ SearchWindow::create()
 
   return dlgSearch;
 }
+/* check to see if we want to set bounds for search */
