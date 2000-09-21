eb96429d68075eeaebf6b888aef066f9cc4b9cb3
Terry Biggs <tbiggs@users.sf.net>
2000-09-21 15:31:34 +0000

    <tbiggs@users.sourceforge.net> menu work
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword@63 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/menustuff.h
+++ gnomesword/menustuff.h
@@ -7,56 +7,51 @@
  ***************************************************************************/
 
  /*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  You should have received a copy of the GNU Library General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- */
- 
- void
-additemtognomemenu		(GtkWidget *MainFrm, 
-									gchar *itemname,
-									gchar *itemdata,
-									gchar *submenuname, 
-									GtkMenuCallback mycallback);
+    *  This program is free software; you can redistribute it and/or modify
+    *  it under the terms of the GNU General Public License as published by
+    *  the Free Software Foundation; either version 2 of the License, or
+    *  (at your option) any later version.
+    *
+    *  This program is distributed in the hope that it will be useful,
+    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *  GNU Library General Public License for more details.
+    *
+    *  You should have received a copy of the GNU Library General Public License
+    *  along with this program; if not, write to the Free Software
+    *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
 
 void
-additemtopopupmenu		(GtkWidget *MainFrm, 
-									GtkWidget *menu, 
-									gchar *itemname, 
-									GtkMenuCallback mycallback);
-void
-addseparator			(GtkWidget *MainFrm,
-									gchar *subtreelabel);
+additemtognomemenu(GtkWidget * MainFrm,
+		   gchar * itemname,
+		   gchar * itemdata,
+		   gchar * submenuname, GtkMenuCallback mycallback);
 
 void
-addsubtreeitem				(GtkWidget *MainFrm,
-									gchar *menulabel, 
-									gchar *subtreelabel);
+additemtopopupmenu(GtkWidget * MainFrm,
+		   GtkWidget * menu,
+		   gchar * itemname, GtkMenuCallback mycallback);
+
+void addseparator(GtkWidget * MainFrm, gchar * subtreelabel);
 
 void
-additemtosubtree			(GtkWidget *MainFrm, 
-									gchar *subtreelabel, 
-									gchar *itemlabel );
-
-GtkWidget *
-additemtooptionmenu		(GtkWidget *MainFrm, 
-									gchar *subtreelabel, 
-									gchar *itemlabel, 
-									GtkMenuCallback mycallback);
+addsubtreeitem(GtkWidget * MainFrm,
+	       gchar * menulabel, gchar * subtreelabel);
 
 void
-removemenuitems				(GtkWidget *MainFrm,
-											gchar *startitem,
-											gint numberofitems);
-											
-											
+additemtosubtree(GtkWidget * MainFrm,
+		 gchar * subtreelabel, gchar * itemlabel);
+
+GtkWidget *additemtooptionmenu(GtkWidget * MainFrm,
+			       gchar * subtreelabel,
+			       gchar * itemlabel,
+			       GtkMenuCallback mycallback);
+
+void
+removemenuitems(GtkWidget * MainFrm,
+		gchar * startitem, gint numberofitems);
+
+GtkWidget *create_pmComments2(GList * mods);
+
+GtkWidget *create_pmDict(GList * mods);
