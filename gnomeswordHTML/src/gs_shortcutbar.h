4348c6ec6275ca37c11af22d28469b71fddd6224
Terry Biggs <tbiggs@users.sf.net>
2001-06-15 14:03:29 +0000

    2001-06-15 added gs_shortcutbar.* <tbiggs@users.sf.net>
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@168 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ gnomeswordHTML/src/gs_shortcutbar.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+                                  gs_shordcutbar.h
+                             -------------------
+    begin                : Thu Jun 14 2001
+    copyright            : (C) 2001 by tbiggs
+    email                : tbiggs@users.sf.net
+ ***************************************************************************/
+
+ /*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include  <gal/shortcut-bar/e-shortcut-bar.h>
+
+void setupSB(GList *text, GList *commentary, GList *dictionary);
+gint add_sb_group(EShortcutBar * shortcut_bar,
+		gchar * group_name);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
