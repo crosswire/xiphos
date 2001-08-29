e252c9e78e130944253b1a9a513e70fcd792935c
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2001-08-29 01:23:43 +0000

    code cleanup - remove files <tbiggs@users.sourceforge.net>
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@248 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/src/gs_preferences_dlg.h
+++ gnomesword/src/gs_preferences_dlg.h
@@ -1,26 +1,31 @@
-/***************************************************************************
-                                gs_preferences_dlg.h
-                             -------------------
-    begin                : Fri July 27 2001
-    copyright            : (C) 2001 by Terry Biggs
-    email                : tbiggs@users.sourceforge.net
- ***************************************************************************/
- 
-/*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+ /*
+    * GnomeSword Bible Study Tool
+    * gs_preferences_dlg.h
+    * -------------------
+    * Fri July 27 2001
+    * copyright (C) 2001 by Terry Biggs
+    * tbiggs@users.sourceforge.net
  */
+ 
+ /*
+    *  This program is free software; you can redistribute it and/or modify
+    *  it under the terms of the GNU General Public License as published by
+    *  the Free Software Foundation; either version 2 of the License, or
+    *  (at your option) any later version.
+    *
+    *  This program is distributed in the hope that it will be useful,
+    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *  GNU Library General Public License for more details.
+    *
+    *  You should have received a copy of the GNU Library General Public License
+    *  along with this program; if not, write to the Free Software
+    *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
+  
+#ifndef __GS_PREFERENCES_DLG_H_
+#define __GS_PREFERENCES_DLG_H_ 
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,4 +41,6 @@ GtkWidget *create_dlgSettings(SETTINGS *s,
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* __GS_PREFERENCES_DLG_H_ */
 
