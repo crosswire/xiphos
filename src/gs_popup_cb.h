72628d9d07aee7821fa48d0b8270e8ae66734d27
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2001-08-29 01:23:43 +0000

    code cleanup - remove files <tbiggs@users.sourceforge.net>
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword@248 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gs_popup_cb.h
+++ src/gs_popup_cb.h
@@ -1,19 +1,31 @@
-/***************************************************************************
-                          gs_popup_cb.h  -  description
-                             -------------------
-    begin                : Thu Feb 8 2001
-    copyright            : (C) 2001 by Terry Biggs
-    email                : tbiggs@users.sourceforge.net
- ***************************************************************************/
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+ /*
+    * GnomeSword Bible Study Tool
+    * gs_popup_cb.h
+    * -------------------
+    * Thu Feb 8 2001
+    * copyright (C) 2001 by Terry Biggs
+    * tbiggs@users.sourceforge.net
+ */
+ 
+ /*
+    *  This program is free software; you can redistribute it and/or modify
+    *  it under the terms of the GNU General Public License as published by
+    *  the Free Software Foundation; either version 2 of the License, or
+    *  (at your option) any later version.
+    *
+    *  This program is distributed in the hope that it will be useful,
+    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *  GNU Library General Public License for more details.
+    *
+    *  You should have received a copy of the GNU Library General Public License
+    *  along with this program; if not, write to the Free Software
+    *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+#ifndef __GS_POPUP_CB_H_
+#define __GS_POPUP_CB_H_ 
 
 #ifdef __cplusplus
 extern "C" {
@@ -93,3 +105,5 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* __GS_POPUP_CB_H_ */
--- src/gs_viewtext_dlg.c
+++ src/gs_viewtext_dlg.c
@@ -1,28 +1,30 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-/***************************************************************************
-gs_viewtext_dlg.c
 
-Sat Mar 24 2001
-copyright  (C) 2001 by Terry Biggs
-tbiggs@users.sourceforge.net
- ***************************************************************************/
-
-/*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  /*
+    * GnomeSword Bible Study Tool
+    * gs_viewtext_dlg.c
+    * -------------------
+    * Sat Mar 24 2001 
+    * copyright (C) 2001 by Terry Biggs
+    * tbiggs@users.sourceforge.net
+    *
  */
-
+ 
+ /*
+    *  This program is free software; you can redistribute it and/or modify
+    *  it under the terms of the GNU General Public License as published by
+    *  the Free Software Foundation; either version 2 of the License, or
+    *  (at your option) any later version.
+    *
+    *  This program is distributed in the hope that it will be useful,
+    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *  GNU Library General Public License for more details.
+    *
+    *  You should have received a copy of the GNU General Public License
+    *  along with this program; if not, write to the Free Software
+    *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
 
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
