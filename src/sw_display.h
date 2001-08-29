66d722c9d32a8137a1a5cba14baa3ae2211a24ff
Terry Biggs <tbiggs@users.sf.net>
2001-08-29 18:05:00 +0000

    code cleanup <tbiggs@users.sourceforge.net>
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword@249 da23c209-522a-4b7c-92e9-24af42be4764

--- src/sw_display.h
+++ src/sw_display.h
@@ -1,30 +1,35 @@
-/***************************************************************************
-sw_display.h
-                             
-Fri Mar 16 2001
-copyright (C) 2001 by Terry Biggs
-tbiggs@users.sourceforge.net
- ***************************************************************************/
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+  /*
+     * GnomeSword Bible Study Tool
+     * sw_display.h
+     * -------------------
+     * Fri Mar 16 2001
+     * copyright (C) 2001 by Terry Biggs
+     * tbiggs@users.sourceforge.net
+     *
+   */
 
-/*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  You should have received a copy of the GNU Library General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- */
+ /*
+    *  This program is free software; you can redistribute it and/or modify
+    *  it under the terms of the GNU General Public License as published by
+    *  the Free Software Foundation; either version 2 of the License, or
+    *  (at your option) any later version.
+    *
+    *  This program is distributed in the hope that it will be useful,
+    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *  GNU Library General Public License for more details.
+    *
+    *  You should have received a copy of the GNU General Public License
+    *  along with this program; if not, write to the Free Software
+    *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
+
+#ifndef __SW_DISPLAY_H_
+#define __SW_DISPLAY_H_
 
 #include <gnome.h>
 #include <swmodule.h>
-
 
 class GtkHTMLEntryDisp:public SWDisplay {
       protected:
@@ -109,4 +114,4 @@ class GTKPerComDisp:public GTKEntryDisp { public:
 	} virtual char Display(SWModule & imodule);
 };
 
-
+#endif /* __SW_DISPLAY_H_ */
