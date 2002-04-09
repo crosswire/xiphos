ad693616d46a39dec90fdc04d59bfbc5fb4f850a
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2002-04-09 20:16:42 +0000

    final commit before 0.7.1
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@365 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/src/sw_shortcutbar.cpp
+++ gnomesword/src/sw_shortcutbar.cpp
@@ -1,15 +1,15 @@
 /* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 
   /*
-    * GnomeSword Bible Study Tool
-    * sw_shortcutbar.cpp
-    * -------------------
-    * Sat June 30 2001
-    * copyright (C) 2001 by Terry Biggs
-    * tbiggs@users.sourceforge.net
-    *
- */
- 
+     * GnomeSword Bible Study Tool
+     * sw_shortcutbar.cpp
+     * -------------------
+     * Sat June 30 2001
+     * copyright (C) 2001 by Terry Biggs
+     * tbiggs@users.sourceforge.net
+     *
+   */
+
  /*
     *  This program is free software; you can redistribute it and/or modify
     *  it under the terms of the GNU General Public License as published by
@@ -42,7 +42,7 @@
 #include <regex.h>
 #include <stdio.h>
 #include <sys/stat.h>
-#include <dirent.h> 
+#include <dirent.h>
 #include <string.h>
 //#include  <gal/shortcut-bar/e-shortcut-bar.h>
 
@@ -56,227 +56,219 @@ extern gint groupnum7;
 extern gchar *shortcutbarDir;
 extern SETTINGS *settings;
 
-list <string> sbfiles;	
+list < string > sbfiles;
 
-static SWDisplay 
-	*viewersbDisplay;	/* to display modules in viewer dialog */
-static SWMgr 
-	*viewersbMgr; 
-static SWModule 
-	*viewersbMod;   /* module for viewer dialog */
+static SWDisplay *viewersbDisplay;	/* to display modules in viewer dialog */
+static SWMgr *viewersbMgr;
+static SWModule *viewersbMod;	/* module for viewer dialog */
 
-gint
-sbtypefromModNameSBSW(gchar *modName)
+gint sbtypefromModNameSBSW(gchar * modName)
 {
-	ModMap::iterator it; //-- iteratior
-	SWMgr *mgr;
-	gint retval = -1;
-	gchar *modType;
-		
-	
-	mgr = new SWMgr();	//-- create sword mgrs
+    ModMap::iterator it;	//-- iteratior
+    SWMgr *mgr;
+    gint retval = -1;
+    gchar *modType;
+
+
+    mgr = new SWMgr();		//-- create sword mgrs
 	/*** get module type ***/
-	//modType = mgr->Modules[modName]->Type();
-	it = mgr->Modules.find(modName);	//-- iterate through the modules until we find modName
-	if (it != mgr->Modules.end()) {	
-		modType = (*it).second->Type();
-		
-		if(!strcmp(modType, "Biblical Texts")){
-			retval = 0;
-		}
-		
-		if(!strcmp(modType, "Commentaries")){
-			retval = 1;
-		}
-		
-		if(!strcmp(modType, "Lexicons / Dictionaries")){
-			retval = 2;
-		} 
-		
-		if(!strcmp(modType, "Generic Books")){
-			retval = 3;
-		} 
+    //modType = mgr->Modules[modName]->Type();
+    it = mgr->Modules.find(modName);	//-- iterate through the modules until we find modName
+    if (it != mgr->Modules.end()) {
+	modType = (*it).second->Type();
+
+	if (!strcmp(modType, "Biblical Texts")) {
+	    retval = 0;
 	}
+
+	if (!strcmp(modType, "Commentaries")) {
+	    retval = 1;
+	}
+
+	if (!strcmp(modType, "Lexicons / Dictionaries")) {
+	    retval = 2;
+	}
+
+	if (!strcmp(modType, "Generic Books")) {
+	    retval = 3;
+	}
+    }
 	/*** delete Sword manager ***/
-	delete mgr;
-	return retval;
+    delete mgr;
+    return retval;
 }
+
+void save_iconsizeSW(gchar * filename, gchar * large_icons)
+{
+    gchar conffile[256];
+    gint j = 0;
+
+    sprintf(conffile, "%s/%s", shortcutbarDir, filename);
+    SWConfig sbInfo(conffile);
+    sbInfo["Shortcut Info"]["Large Icon"] = large_icons;
+    sbInfo.Save();
+}
+
+
+GList *loadshortcutbarSW(gchar * filename, gchar * group_name,
+			 gchar * use_largeicons)
+{
+    SectionMap::iterator sit;
+    ConfigEntMap::iterator eit;
+    SWConfig *sbconf;
+    ConfigEntMap emap;
+    gchar *buf, conffile[256];
+    gint j = 0;
+    GList *list;
+
+    list = NULL;
+    sprintf(conffile, "%s/%s", shortcutbarDir, filename);
+
+    SWConfig sbInfo(conffile);
+    sprintf(group_name, "%s",
+	    sbInfo["Shortcut Info"]["Group Name"].c_str());
+    sprintf(use_largeicons, "%s",
+	    sbInfo["Shortcut Info"]["Large Icon"].c_str());
+    sbInfo.Save();
+
+    sbconf = new SWConfig(conffile);
+    if ((sit = sbconf->Sections.find("ROOT")) != sbconf->Sections.end()) {
+	if ((eit = (*sit).second.begin()) != (*sit).second.end()) {
+	    for (eit = (*sit).second.begin(); eit != (*sit).second.end();
+		 eit++) {
+		buf = g_strdup((char *) (*eit).second.c_str());
+		list = g_list_append(list, buf);
+	    }
+	}
+    }
+    delete sbconf;
+    return list;
+}
+
 
 void
-save_iconsizeSW(gchar *filename, gchar *large_icons)
+saveshortcutbarSW(gchar * filename, gchar * group_name, gint group_num,
+		  gchar * large_icons)
 {
-	gchar 
-		conffile[256];
-	gint 
-		j=0;
-	
-	sprintf(conffile,"%s/%s", shortcutbarDir, filename);	
-	SWConfig sbInfo(conffile);
-	sbInfo["Shortcut Info"]["Large Icon"] = large_icons;	
-	sbInfo.Save();
+    SectionMap::iterator sit;
+    ConfigEntMap::iterator eit;
+    SWConfig *sbconf;
+    ConfigEntMap emap;
+    gchar buf[500], conffile[256], *item_url, *item_name;
+    gint j = 0, number_of_items;
+
+    sprintf(conffile, "%s/%s", shortcutbarDir, filename);
+    unlink(conffile);
+
+    SWConfig sbInfo(conffile);
+    sbInfo["Shortcut Info"]["Group Name"] = group_name;
+    sbInfo["Shortcut Info"]["Large Icon"] = large_icons;
+    sbInfo.Save();
+
+    sbconf = new SWConfig(conffile);
+    emap = sbconf->Sections["ROOT"];
+
+    number_of_items =
+	gs_shortcut_model_get_num_items(settings->shortcut_bar, group_num);
+    //e_shortcut_model_get_num_items(E_SHORTCUT_BAR(settings->shortcut_bar)->model, group_num);
+
+    for (j = 0; j < number_of_items; j++) {
+	gs_shortcut_model_get_item_info(settings->shortcut_bar,
+					group_num,
+					j, &item_url, &item_name);
+	/*e_shortcut_model_get_item_info(E_SHORTCUT_BAR
+	   (settings->shortcut_bar)->model,
+	   group_num,
+	   j, &item_url, &item_name,
+	   NULL); */
+	sprintf(buf, "branch%d", j);
+	emap.erase(buf);
+	emap.insert(ConfigEntMap::value_type(buf, (gchar *) item_name));
+	g_print("saving list item: %s\n", (gchar *) item_name);
+    }
+
+    sbconf->Sections["ROOT"] = emap;
+    sbconf->Save();
+    delete sbconf;
 }
 
-
-GList *loadshortcutbarSW(gchar *filename, gchar *group_name, gchar *use_largeicons)
+GList *getModlistSW(gchar * modtype)
 {
-	SectionMap::iterator sit;
-	ConfigEntMap::iterator eit;
-	SWConfig *sbconf;
-	ConfigEntMap emap;
-	gchar 
-		*buf,
-		conffile[256];
-	gint j=0;
-	GList *list;
-	
-	list = NULL;
-	sprintf(conffile,"%s/%s", shortcutbarDir, filename);
-	
-	SWConfig sbInfo(conffile);
-	sprintf(group_name,"%s", sbInfo["Shortcut Info"]["Group Name"].c_str());
-	sprintf(use_largeicons,"%s",  sbInfo["Shortcut Info"]["Large Icon"].c_str());	
-	sbInfo.Save();
-	
-	sbconf = new SWConfig(conffile);	
-	if ((sit = sbconf->Sections.find("ROOT")) != sbconf->Sections.end()) {
-		if ((eit = (*sit).second.begin()) != (*sit).second.end()) {	
-			for(eit = (*sit).second.begin(); eit != (*sit).second.end(); eit++){					
-				buf = g_strdup((char*)(*eit).second.c_str());
-				list = g_list_append(list,buf);
-			}
-		}
+    GList *list;
+    ModMap::iterator it;
+    SWMgr *mgr;
+    gchar *buf;
+
+    mgr = new SWMgr();		//-- create sword mgrs
+    list = NULL;
+    for (it = mgr->Modules.begin(); it != mgr->Modules.end(); it++) {
+	if (!strcmp((*it).second->Type(), modtype)) {
+	    buf = g_strdup((gchar *) (*it).second->Description());
+	    list = g_list_append(list, buf);	/* calling function must free each item */
 	}
-	delete sbconf;
-	return list;
-}
-	
-
-void
-saveshortcutbarSW(gchar *filename, gchar *group_name, gint group_num, gchar *large_icons)
-{	
-	SectionMap::iterator sit;
-	ConfigEntMap::iterator eit;
-	SWConfig *sbconf;
-	ConfigEntMap emap;
-	gchar 
-		buf[500],
-		conffile[256],
-		*item_url,
-		*item_name;
-	gint 
-		j=0,
-		number_of_items;
-	
-	sprintf(conffile,"%s/%s", shortcutbarDir, filename);
-	unlink(conffile);
-	
-	SWConfig sbInfo(conffile);
-	sbInfo["Shortcut Info"]["Group Name"] = group_name;
-	sbInfo["Shortcut Info"]["Large Icon"] = large_icons;	
-	sbInfo.Save();
-	
-	sbconf = new SWConfig(conffile);
-	emap = sbconf->Sections["ROOT"];
-	
-	number_of_items = gs_shortcut_model_get_num_items(settings->shortcut_bar, 
-						group_num);	
-	//e_shortcut_model_get_num_items(E_SHORTCUT_BAR(settings->shortcut_bar)->model, group_num);
-	
-	for (j = 0; j < number_of_items; j++) {
-		gs_shortcut_model_get_item_info(settings->shortcut_bar,
-						group_num,
-						j, &item_url, &item_name);
-		/*e_shortcut_model_get_item_info(E_SHORTCUT_BAR
-					       (settings->shortcut_bar)->model,
-					       group_num,
-					       j, &item_url, &item_name,
-					       NULL);*/
-		sprintf(buf, "branch%d", j);
-		emap.erase(buf); emap.insert(ConfigEntMap::value_type(buf, (gchar *) item_name));	
-		g_print("saving list item: %s\n",(gchar*)item_name);
-	}	
-	
-	sbconf->Sections["ROOT"] = emap;
-	sbconf->Save();
-	delete sbconf;  
-}
-
-GList *getModlistSW(gchar *modtype)
-{
-	GList *list;
-	ModMap::iterator it;	
-	SWMgr *mgr;
-	gchar *buf;
-	
-	mgr = new SWMgr();	//-- create sword mgrs
-	list = NULL;
-	for(it = mgr->Modules.begin(); it != mgr->Modules.end(); it++){
-		if(!strcmp((*it).second->Type(), modtype)){
-			buf = g_strdup((gchar*)(*it).second->Description());
-			list = g_list_append(list,buf);	/* calling function must free each item */
-		}
-	}
-	delete mgr;				
-	return list;
+    }
+    delete mgr;
+    return list;
 }
 
 /*
  * 
  * 
  */
-gboolean
-displaydictlexSBSW(gchar *modName, gchar *key, SETTINGS *s)
+gboolean displaydictlexSBSW(gchar * modName, gchar * key, SETTINGS * s)
 {
-	gchar 
-		buf[256], 
-		*utf8str,
-		tmpbuf[256];	
-	
-	gtk_notebook_set_page(GTK_NOTEBOOK(lookup_widget(s->app, "nbVL")), 2);	
-	sprintf(s->groupName,"%s","Viewer");
+    if (s->showshortcutbar) {
+	gchar buf[256], *utf8str, tmpbuf[256];
+
+	gtk_notebook_set_page(GTK_NOTEBOOK(lookup_widget(s->app, "nbVL")),
+			      2);
+	sprintf(s->groupName, "%s", "Viewer");
 	showSBVerseList(s);
-	//changegroupnameSB(s, s->groupName, groupnum7);
-	if(!strcmp(modName,viewersbMod->Name())){
-		viewersbMod->SetKey(key); //-- set key to the first one in the list
-		viewersbMod->Display(); 
-	}else{
-		ModMap::iterator it; 	
-		it = viewersbMgr->Modules.find(modName); //-- iterate through the modules until we find modName - modName was passed by the callback
-		if (it != viewersbMgr->Modules.end()){ //-- if we find the module	
-			viewersbMod = (*it).second;  //-- change module to new module
-			viewersbMod->SetKey(key); //-- set key to the first one in the list
-			viewersbMod->Display(); 
-		}
+
+	if (!strcmp(modName, viewersbMod->Name())) {
+	    viewersbMod->SetKey(key);	//-- set key to the first one in the list
+	    viewersbMod->Display();
 	}
-	return TRUE;	
+
+	else {
+	    ModMap::iterator it;
+	    it = viewersbMgr->Modules.find(modName);	//-- iterate through the modules until we find modName - modName was passed by the callback
+	    if (it != viewersbMgr->Modules.end()) {	//-- if we find the module       
+		viewersbMod = (*it).second;	//-- change module to new module
+		viewersbMod->SetKey(key);	//-- set key to the first one in the list
+		viewersbMod->Display();
+	    }
+	}
+	return TRUE;
+    }
+    return FALSE;
 }
 
 /****************************************************************************************
  *setupVLSWORD - set up the sword stuff for the viewer dialog
  ****************************************************************************************/
-void setupviewerSBSW(GtkWidget *html_widget)
-{	
-	ModMap::iterator it; //-- iteratior	
-	SectionMap::iterator sit; //-- iteratior
-	
-	viewersbMgr	= new SWMgr(new MarkupFilterMgr(FMT_HTMLHREF));	//-- create sword mgrs
-	viewersbMod     = NULL;
-	viewersbDisplay = new  GtkHTMLEntryDisp(html_widget,settings);
-	
-	for(it = viewersbMgr->Modules.begin(); it != viewersbMgr->Modules.end(); it++){
-		viewersbMod = (*it).second;
-		if(!strcmp((*it).second->Type(), "Lexicons / Dictionaries")){
-			viewersbMod->Disp(viewersbDisplay);
-		}
+void setupviewerSBSW(GtkWidget * html_widget)
+{
+    ModMap::iterator it;	//-- iteratior     
+    SectionMap::iterator sit;	//-- iteratior
+
+    viewersbMgr = new SWMgr(new MarkupFilterMgr(FMT_HTMLHREF));	//-- create sword mgrs
+    viewersbMod = NULL;
+    viewersbDisplay = new GtkHTMLEntryDisp(html_widget, settings);
+
+    for (it = viewersbMgr->Modules.begin();
+	 it != viewersbMgr->Modules.end(); it++) {
+	viewersbMod = (*it).second;
+	if (!strcmp((*it).second->Type(), "Lexicons / Dictionaries")) {
+	    viewersbMod->Disp(viewersbDisplay);
 	}
+    }
 }
 
 /*** close down viewer dialog ***/
-void shutdownviewerSBSW(void) 
-{	
-	delete viewersbMgr;	
-	if(viewersbDisplay)
-		delete viewersbDisplay;	
+void shutdownviewerSBSW(void)
+{
+    delete viewersbMgr;
+    if (viewersbDisplay)
+	delete viewersbDisplay;
 }
-
-
