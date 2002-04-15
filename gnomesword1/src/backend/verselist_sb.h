195eacccf73c5964533a9fbad73c8b7d0aca470c
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2002-04-15 16:03:11 +0000

    Initial revision
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@374 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ gnomesword1/src/backend/verselist_sb.h
@@ -0,0 +1,50 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+  /*
+     * GnomeSword Bible Study Tool
+     * sw_verselist_sb.h
+     * -------------------
+     * Thur Aug 02 2001
+     * copyright (C) 2001 by Terry Biggs
+     * tbiggs@users.sourceforge.net
+     *
+   */
+
+ /*
+    *  This program is free software; you can redistribute it and/or modify
+    *  it under the terms of the GNU General Public License as published by
+    *  the Free Software Foundation; either version 2 of the License, or
+    *  (at your option) any later version.
+    *
+    *  This program is distributed in the hope that it will be useful,
+    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *  GNU Library General Public License for more details.
+    *
+    *  You should have received a copy of the GNU General Public License
+    *  along with this program; if not, write to the Free Software
+    *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
+
+#ifndef __SW_VERSELIST_SB_H__
+#define __SW_VERSELIST_SB_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <gnome.h>
+#include "gs_gnomesword.h"
+
+	gboolean getVerseListSBSWORD(gchar * modName,
+				     	gchar * vlist, 
+					SETTINGS * s);
+	void setupVerseListSBSWORD(GtkWidget * html_widget);
+	void shutdownverselistSBSWORD(void);
+	void changeVerseListSBSWORD(SETTINGS * s, 
+					gchar * url);
+
+#ifdef __cplusplus
+}
+#endif
+#endif				/* __SW_VERSELIST_SB_H__ */
