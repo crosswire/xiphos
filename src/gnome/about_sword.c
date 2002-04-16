96a3a42b0e24f9e8e09993b648ab3a2e81e37182
japas <japas@da23c209-522a-4b7c-92e9-24af42be4764>
2002-04-16 19:01:45 +0000

    Little reformating and changed head.
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword1@382 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome/about_sword.c
+++ src/gnome/about_sword.c
@@ -1,39 +1,32 @@
-
- /*
-    GnomeSword Bible Study Tool
-    *  about_sword.c - About dialogs for GnomeSword
-    *
-    *  Copyright (C) 2000,2001,2002 GnomeSword Developer Team
-    *
-    *  This program is free software; you can redistribute it and/or modify
-    *  it under the terms of the GNU General Public License as published by
-    *  the Free Software Foundation; either version 2 of the License, or
-    *  (at your option) any later version.
-    *
-    *  This program is distributed in the hope that it will be useful,
-    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-    *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    *  GNU Library General Public License for more details.
-    *
-    *  You should have received a copy of the GNU General Public License
-    *  along with this program; if not, write to the Free Software
-    *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-  */
+/*
+ * GnomeSword Bible Study Tool
+ * about_sword.c - Sword about dialog
+ *
+ * Copyright (C) 2000,2001,2002 GnomeSword Developer Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 
 #ifdef HAVE_CONFIG_H
-#  include <config.h>
+#include <config.h>
 #endif
-/*
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
-*/
+
 #include <gnome.h>
 #include <gtkhtml/gtkhtml.h>
 
 #include "about_sword.h"
-
 
 /******************************************************************************
  * privat
@@ -216,5 +209,3 @@ GtkWidget *gui_create_about_sword(void)
 
         return about_sword;
 }
-
-/******  end of file  ******/
