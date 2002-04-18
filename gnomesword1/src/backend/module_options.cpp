0dcd852bae0b966715e7ce8c19305e501d580d56
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2002-04-18 12:32:24 +0000

    more code cleanup
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@390 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword1/src/backend/module_options.cpp
+++ gnomesword1/src/backend/module_options.cpp
@@ -1,30 +1,24 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 
-  /*
-    * GnomeSword Bible Study Tool
-    * sw_module_options.cpp
-    * -------------------
-    * Thu Nov 15 13:20:56 2001
-    * copyright (C) 2001 by Terry Biggs
-    * tbiggs@users.sourceforge.net
-    *
+/*
+ * GnomeSword Bible Study Tool
+ * module_options.cpp - load and save individual module options
+ *
+ * Copyright (C) 2000,2001,2002 GnomeSword Developer Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
- 
- /*
-    *  This program is free software; you can redistribute it and/or modify
-    *  it under the terms of the GNU General Public License as published by
-    *  the Free Software Foundation; either version 2 of the License, or
-    *  (at your option) any later version.
-    *
-    *  This program is distributed in the hope that it will be useful,
-    *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-    *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    *  GNU Library General Public License for more details.
-    *
-    *  You should have received a copy of the GNU General Public License
-    *  along with this program; if not, write to the Free Software
-    *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-  */
 
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
@@ -56,45 +50,37 @@ extern SETTINGS *settings;
 /******************************************************************************
  * load module option - using sword SWConfig
  ******************************************************************************/
-bool load_module_options(gchar *modName, gchar *option)
+bool load_module_options(gchar * modName, gchar * option)
 {
 	gchar buf[255], *yesno;
 	bool retval = false;
-	
+
 	sprintf(buf, "%s/modops.conf", gSwordDir);
 	SWConfig module_options(buf);
-	module_options.Load();	
-	yesno = (gchar*)module_options[modName][option].c_str();
-	if(!strcmp(yesno,"On"))
+	module_options.Load();
+	yesno = (gchar *) module_options[modName][option].c_str();
+	if (!strcmp(yesno, "On"))
 		retval = true;
 	else
 		retval = false;
-	return retval;	
+	return retval;
 }
- 
+
 /******************************************************************************
  * save module option - using sword SWConfig
  ******************************************************************************/
-bool save_module_options(gchar *modName, gchar *option, gchar *value)
+bool save_module_options(gchar * modName, gchar * option, gchar * value)
 {
 	gchar buf[80], buf2[255];
 
 	sprintf(buf, "%s/modops.conf", gSwordDir);
 	SWConfig module_options(buf);
-	
-	module_options[modName][option] = value;	
-	
+
+	module_options[modName][option] = value;
+
 	module_options.Save();
 	return true;
 }
 
-/******************************************************************************
- * save module font - using sword SWConfig
- ******************************************************************************/
-bool save_module_font(gchar *modName, gchar *font)
-{
-	
-	return false;
-}
 
-
+/******  end of file  ******/
--- gnomesword1/src/main/gs_gui.h
+++ gnomesword1/src/main/gs_gui.h
@@ -33,7 +33,7 @@ extern "C" {
 #endif
 #include "gs_gnomesword.h"
 
-	GtkWidget *create_mainwindow(SETTINGS *s);
+	void create_mainwindow(SETTINGS *s);
 
 #ifdef __cplusplus
 }
