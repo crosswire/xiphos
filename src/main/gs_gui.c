c9a4f111903f7122fd1bb64cf454536cd409c701
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2002-04-20 16:29:29 +0000

    added Spanish trans - es.po
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword1@395 da23c209-522a-4b7c-92e9-24af42be4764

--- src/main/gs_gui.c
+++ src/main/gs_gui.c
@@ -151,7 +151,7 @@ static GnomeUIInfo file1_menu_uiinfo[] = {
 static GnomeUIInfo edit1_menu_uiinfo[] = {
 	{
 	 GNOME_APP_UI_ITEM, N_("Copy"),
-	 N_("Copy highlighted text form main window"),
+	 N_("Copy highlighted text from main window"),
 	 on_copyhtml_activate, "htmlTexts", NULL,
 	 GNOME_APP_PIXMAP_NONE, NULL,
 	 0, 0, NULL},
@@ -651,7 +651,7 @@ void create_mainwindow(SETTINGS *s)
 	    gtk_toolbar_append_element(GTK_TOOLBAR(toolbarOptions),
 				       GTK_TOOLBAR_CHILD_TOGGLEBUTTON,
 				       NULL, "Strongs",
-				       _("Toogle Strongs Numbers"), NULL,
+				       _("Toggle Strongs Numbers"), NULL,
 				       tmp_toolbar_icon, NULL, NULL);
 	gtk_widget_ref(btnStrongs);
 	gtk_object_set_data_full(GTK_OBJECT(s->app), "btnStrongs",
@@ -665,7 +665,7 @@ void create_mainwindow(SETTINGS *s)
 	    gtk_toolbar_append_element(GTK_TOOLBAR(toolbarOptions),
 				       GTK_TOOLBAR_CHILD_TOGGLEBUTTON,
 				       NULL, "Morphological",
-				       _("Toogle Morphological Tags"), NULL,
+				       _("Toggle Morphological Tags"), NULL,
 				       tmp_toolbar_icon, NULL, NULL);
 	gtk_widget_ref(btnMorphs);
 	gtk_object_set_data_full(GTK_OBJECT(s->app), "btnMorphs",
@@ -679,7 +679,7 @@ void create_mainwindow(SETTINGS *s)
 	    gtk_toolbar_append_element(GTK_TOOLBAR(toolbarOptions),
 				       GTK_TOOLBAR_CHILD_TOGGLEBUTTON,
 				       NULL, "Footnotes",
-				       _("Toogle Footnotes"), NULL,
+				       _("Toggle Footnotes"), NULL,
 				       tmp_toolbar_icon, NULL, NULL);
 	gtk_widget_ref(btnFootnotes);
 	gtk_object_set_data_full(GTK_OBJECT(s->app), "btnFootnotes",
