2e353c8a727f430f277b984177fcb7f1e7ed15d5
Terry Biggs <tbiggs@users.sf.net>
2002-05-10 20:30:28 +0000

    add file
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@438 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ gnomesword1/src/main/find_dlg_defs.h
@@ -0,0 +1,22 @@
+
+#ifndef _FIND_DLG_DEFS_H_
+#define _FIND_DLG_DEFS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif	
+
+struct _gs_find_dialog {
+	GnomeDialog *dialog;
+	GtkWidget *htmlwidget;
+	GtkWidget *entry;
+	GtkWidget *backward;
+	GtkWidget *case_sensitive;
+	gboolean regular;
+};	
+	
+	
+#ifdef __cplusplus
+}
+#endif
+#endif /* _FIND_DLG_DEFS_H_ */
