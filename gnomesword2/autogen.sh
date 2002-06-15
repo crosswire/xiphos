c45f9f47a45f7035d47ae4e7e5aa1db23f3042f0
Scott Bell <skaught@users.sourceforge.net>
2002-06-15 19:44:32 +0000

    Moving over to modern GNOME2 build environment scripts and macros
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@471 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword2/autogen.sh
+++ gnomesword2/autogen.sh
@@ -4,12 +4,19 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="the package."
+PKG_NAME="Gnome Hello Demonstration Application"
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/configure.in \
+  && test -f $srcdir/ChangeLog \
+  && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level directory"
+    echo " top-level gnome directory"
     exit 1
 }
 
-. $srcdir/macros/autogen.sh
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
