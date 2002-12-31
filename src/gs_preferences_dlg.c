2d75a1edfdf915f1599e4b4c721cc1cbe2daf72d
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2002-12-31 18:41:49 +0000

    fixed crash when no books are present
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword@574 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gs_preferences_dlg.c
+++ src/gs_preferences_dlg.c
@@ -864,7 +864,7 @@ GtkWidget *create_dlgSettings(SETTINGS * s,
 	cmbVerseNumSize_items =
 	    g_list_append(cmbVerseNumSize_items, (gpointer) _("+4"));
 	cmbVerseNumSize_items =
-	    g_list_append(cmbVerseNumSize_items, (gpointer) _("+5\r"));
+	    g_list_append(cmbVerseNumSize_items, (gpointer) _("+5"));
 	gtk_combo_set_popdown_strings(GTK_COMBO(cmbVerseNumSize),
 				      cmbVerseNumSize_items);
 	g_list_free(cmbVerseNumSize_items);
