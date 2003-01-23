965518f5af6f3e01ab3ea820208eae0c92ffc0d1
Terry Biggs <tbiggs@users.sf.net>
2003-01-23 05:18:23 +0000

    added gui/bibletext_menu.h
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword1@589 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ src/gui/bibletext_menu.h
@@ -0,0 +1,36 @@
+/*
+ * GnomeSword Bible Study Tool
+ * bibletext.h - gui for Bible text modules
+ *
+ * Copyright (C) 2000,2001,2002 GnomeSword Developer Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef ___BIBLETEXT_MENU_H_
+#define ___BIBLETEXT_MENU_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+GtkWidget *gui_create_pm_text(TEXT_DATA * t);
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
