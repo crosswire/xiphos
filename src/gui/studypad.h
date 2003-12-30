5f96a487c402c4ef58133e9f62f0f0a901b9cf0e
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2003-12-30 16:22:51 +0000

    added space to nav toolbar and work on spell dialog
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword1@868 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gui/studypad.h
+++ src/gui/studypad.h
@@ -27,7 +27,7 @@
 
 extern GSHTMLEditorControlData *editor_cd;
 
-void gui_studypad_can_close(GSHTMLEditorControlData *ecd);
+void gui_studypad_can_close(void);
 gint save_file_program_end(GtkWidget *htmlwidget, gchar *filename);
 gint save_file(gchar *filename, GSHTMLEditorControlData *ecd);
 gint load_file(gchar *filename, GSHTMLEditorControlData *ecd);
