a60bf13b11cdf1e801c66b10bc571a9f7a26a65e
Terry Biggs <tbiggs@users.sf.net>
2004-02-07 18:49:25 +0000

    add files gnome2/url.c and gui/url.h
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@953 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword1/src/gui/url.h
+++ gnomesword1/src/gui/url.h
@@ -1,6 +1,6 @@
 /*
  * GnomeSword Bible Study Tool
- * utilities.h - support functions
+ * url.h - support functions
  *
  * Copyright (C) 2000,2001,2002 GnomeSword Developer Team
  *
@@ -19,28 +19,13 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-	
-#include <gnome.h>
-#include "main/configs.h"
+#ifndef __URL_H_
+#define __URL_H_
 
-void gui_load_module_tree(GtkWidget * tree, gboolean is_sidebar);
-MOD_FONT *get_font(gchar * mod_name);
-void free_font(MOD_FONT *mf);
-gchar * remove_linefeeds(gchar * buf);	
-void gui_add_item2gnome_menu(GtkWidget * MainFrm, gchar * itemname,
-     gchar * itemdata, gchar * submenuname, GCallback mycallback);
-void gui_add_separator2menu(GtkWidget * MainFrm, gchar * subtreelabel);
-void gui_add_mods_to_menus(GList * modlist, gchar * menu,
-					GCallback callback);
-void gui_remove_menu_items(gchar *startitem, gint numberofitems);
-void gui_add_mods_2_gtk_menu(gint mod_type, GtkWidget * menu,
-				GCallback callback);
-GList *gui_fill_count_list(int count);
-void gui_free_count_list(GList *glist);
+#include <gnome.h>
+//#include "main/configs.h"
+
+gint gui_url_handler(const gchar * url);
 	
-#ifdef __cplusplus
-}
+
 #endif
