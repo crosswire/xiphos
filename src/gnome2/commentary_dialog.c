8ee7e3b3fe6857cfdfa472e28bea3d2f10dfa600
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2004-10-22 22:52:01 +0000

    worked on gtkhtml-3.1 stuff
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword1@1032 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome2/commentary_dialog.c
+++ src/gnome2/commentary_dialog.c
@@ -25,10 +25,11 @@
 
 #include <gnome.h>
 #include <gtkhtml/gtkhtml.h>
+/*
 #ifdef USE_GTKHTML30
 #include <gal/widgets/e-unicode.h>
 #endif
-
+*/
 #include "gui/commentary_dialog.h"
 #include "gui/dialog.h"
 #include "gui/html.h"
