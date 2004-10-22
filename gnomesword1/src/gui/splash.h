2132e8d48c86ab35abc74081a275b1765a483789
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2004-10-22 22:50:51 +0000

    fixed to work with gtkhtml-3.1 I hope
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@1031 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword1/src/gui/splash.h
+++ gnomesword1/src/gui/splash.h
@@ -1,6 +1,31 @@
 #ifndef _GUI_SPLASH_H_
 #define _GUI_SPLASH_H_
 
+
+#define E_MAKE_TYPE(l,str,t,ci,i,parent) \
+GtkType l##_get_type(void)\
+{\
+	static GType type = 0;				\
+	if (!type){					\
+		static GTypeInfo const object_info = {	\
+			sizeof (t##Class),		\
+							\
+			(GBaseInitFunc) NULL,		\
+			(GBaseFinalizeFunc) NULL,	\
+							\
+			(GClassInitFunc) ci,		\
+			(GClassFinalizeFunc) NULL,	\
+			NULL,		\
+							\
+			sizeof (t),			\
+			0,		\
+			(GInstanceInitFunc) i,		\
+		};					\
+		type = g_type_register_static (parent, str, &object_info, 0);	\
+	}						\
+	return type;					\
+}
+
 void gui_splash_init(void);
 void gui_splash_step1(void);
 void gui_splash_step2(void);
