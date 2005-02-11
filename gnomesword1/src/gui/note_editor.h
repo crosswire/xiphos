274c7116ac88193ae6ccbe4dc9f6791548b432f6
Terry Biggs <tbiggs@users.sf.net>
2005-02-11 02:20:57 +0000

    add files
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@1104 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ gnomesword1/src/gui/note_editor.h
@@ -0,0 +1,38 @@
+/*
+ * GnomeSword Bible Study Tool
+ * editor_menu.h - popup menu for html editor
+ *
+ * Copyright (C) 2000,2001,2002 GnomeSword Developer Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __NOTE_EDITOR_H_
+#define __NOTE_EDITOR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <gnome.h>
+#include "main/module_dialogs.h"
+
+void gui_create_note_editor(DIALOG_DATA * d);
+
+#ifdef __cplusplus
+}
+#endif	
+
+#endif
