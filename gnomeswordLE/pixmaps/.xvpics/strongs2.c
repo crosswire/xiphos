e62a417deae70feccff84f4441dd6a49131b6bf4
(no author) <(no author)@da23c209-522a-4b7c-92e9-24af42be4764>
2005-05-31 02:02:20 +0000

    This commit was manufactured by cvs2svn to create tag 'unstable_2_1_2_rel'.
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/tags/unstable_2_1_2_rel@1148 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomeswordLE/pixmaps/.xvpics/strongs2.c
+++
@@ -1,5 +0,0 @@
-P7 332
-#IMGINFO:25x25 RGB (6263 bytes)
-#END_OF_COMMENTS
-25 25 255
-I$IIÛ¶¶¶IH%IÛ¶¶Ú%HI%Û¶Ú¶%I$IIÛ¶¶¶IH%IÛ¶¶Ú%HI%Û¶Ú¶%I$IIÛ¶¶¶IH%IÛ¶¶Ú%HI%Û¶Ú¶%I$IIÛ¶¶¶IH%IÛ¶¶Ú%HI%Û¶Ú¶%Û¶¶¶I$IIÛ¶¶Ú%H%IÛ¶Ú¶%HI%ÛÛ¶¶¶I$Iäáº×¶(IE(áä»Ö$I)IÖÛ¶¶¶I$åà»Öº¶%HII×Àä»D)EHÛÛ¶¶¶Iàà)Û¶ºÖ%H)I×ºàà)IH%ÛI$IIàä»¶EH)EÛº¶Ö%HIáä»¶¶DI$Iàåº×¶(EI(ÛÖ·º$IEIäà»¶DI$åà»Ör&m&nŽ(×àä»$Iàà)Û¶rrH‘q×(‘%Û¶àäNÛÀnÚ$IqÛ¶¶‘q×rÖ)H%änÛÀnÚ$IqÛ¶¶‘%HŽnÚ$I)àrÛÀàr$I»Ö¶qI$'rrÖ$IäárÛ¶àÀrDnÚ¶»nÚmÚ%äàr·I$IàånºÖ)DI)Û¶Ö¶)HEIàäOÚ$I$IIàäoºD)IDÛ¶»Ö$I)àän»Ö$I$IIÛàÀrD)IIÖ»¶Ö$IåàRÖ¶Û$I$IIÛ¶ààRDI)Û¶Ö¶)àän»Ö¶ºEÛ¶¶¶I$Iäár¶Ö)DI)àäoºD)IH×Û¶¶¶I$IIÛ¶¶Ú%H%IÛ¶Ú¶%HI%ÛÛ¶¶¶I$IIÛ¶¶Ú%H%IÛ¶Ú¶%HI%ÛÛ¶¶¶I$IIÛ¶¶Ú%H%IÛ¶Ú¶%HI%ÛI$IIÛ¶¶¶IH%IÛ¶¶Ú%HI%Û¶Ú¶%
