5413f59dfe70fb2687bde2f1d62d2359edab26b1
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2005-07-09 00:21:20 +0000

    added prefs.glade to use libglade for prefernces dialog
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@1151 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword1/src/main/embed.h
+++ gnomesword1/src/main/embed.h
@@ -31,6 +31,7 @@ extern "C"
 #endif
 #ifdef USE_GTKMOZEMBED
 	
+void embed_print(gboolean preview, GtkMozEmbed *embed);
 void embed_copy_selection(GtkMozEmbed *mozilla_embed);
 void embed_go_to_anchor(GtkMozEmbed *mozilla_embed, const char *anchor);
 void embed_navigate_to_anchor (GtkMozEmbed *mozilla_embed, const char *anchor);
