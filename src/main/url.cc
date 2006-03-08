d989187fa82b999bd20684c2cc34428e6aa7724f
Terry Biggs <tbiggs@users.sf.net>
2006-03-08 00:34:23 +0000

    learning
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword1@1201 da23c209-522a-4b7c-92e9-24af42be4764

--- src/main/url.cc
+++ src/main/url.cc
@@ -2,7 +2,7 @@
  * GnomeSword Bible Study Tool
  * url.cc - support functions
  *
- * Copyright (C) 2004 GnomeSword Developer Team
+ * Copyright (C) 2004,2005,2006 GnomeSword Developer Team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
