e0001266008a9a3e1db471a600503a4f790fc86e
Terry Biggs <tbiggs@users.sf.net>
2006-03-28 21:26:28 +0000

    fixed bug 1419036 'clicking "view context" in parallel view segfaults'
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@1210 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/src/gui/widgets.h
+++ gnomesword/src/gui/widgets.h
@@ -105,7 +105,7 @@ struct  _app_widgets {
                 *viewdicts_item, /* widget to access toggle menu - for showdicts */
 		*versestyle_item,	/* widget to access toggle menu - for versestyle */
 		*button_dict_book,      /* main toolbar togglebutton */
-		*button_parallel_view,  /* main toolbar togglebutton */
+//		*button_parallel_view,  /* main toolbar togglebutton */
 		*button_new_tab,	/* creates a new passage tab */
 		*hboxtb;		/* container for browsing notebook and button */
 };
