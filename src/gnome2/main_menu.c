ceb978f777bfde249ce01b4f9f834736f2bbf913
domcox <domcox@da23c209-522a-4b7c-92e9-24af42be4764>
2006-12-04 17:59:42 +0000

    migrating help system to gnome-doc-utils
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword@1310 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome2/main_menu.c
+++ src/gnome2/main_menu.c
@@ -96,7 +96,7 @@ void on_help_contents_activate(GtkMenuItem * menuitem,
 {	
 	GError *error = NULL;
 	
-	if (gnome_help_display ((const gchar*)"gnomesword-manual.xml", 
+	if (gnome_help_display ((const gchar*)"gnomesword.xml", 
 			NULL, &error) == FALSE) {
 		g_warning(error->message);
 		g_error_free (error);        
