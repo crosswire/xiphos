9d069f5c676826d9714fa5ac9701391831efe93f
Karl Kleinpaste <karl@kleinpaste.org>
2007-01-22 01:56:01 +0000

    feat.req #1193235, double-space scaffolding
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk/gnomesword@1462 da23c209-522a-4b7c-92e9-24af42be4764

--- src/main/xml.c
+++ src/main/xml.c
@@ -590,6 +590,7 @@ int xml_create_settings_file(char *path)
 	xmlNewTextChild(section_node, NULL, (const xmlChar *)"showversenum", (const xmlChar *)"1");
 	xmlNewTextChild(section_node, NULL, (const xmlChar *)"readaloud", (const xmlChar *)"0");
 	xmlNewTextChild(section_node, NULL, (const xmlChar *)"versehighlight", (const xmlChar *)"0");
+	xmlNewTextChild(section_node, NULL, (const xmlChar *)"doublespace", (const xmlChar *)"0");
 	xmlNewTextChild(section_node, NULL, (const xmlChar *)"setup_canceled",(const xmlChar *)"0");
 
 	section_node = xmlNewChild(root_node, NULL, (const xmlChar *)"modules", NULL);
