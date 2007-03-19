8c16dc2806dec0ac0d50cd7244be15959a50405a
domcox <domcox@da23c209-522a-4b7c-92e9-24af42be4764>
2007-03-19 21:23:19 +0000

    i18n update
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@1680 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/src/gnome2/bookmarks_treeview.c
+++ gnomesword/src/gnome2/bookmarks_treeview.c
@@ -99,10 +99,10 @@ void gui_verselist_to_bookmarks(GList * verses)
 	gtk_tree_model_get_iter_first(GTK_TREE_MODEL(model), &iter);
 	info = gui_new_dialog();
 	info->stock_icon = GTK_STOCK_OPEN;
-	info->title = N_("Bookmark");
-	info->label_top = N_("Enter Folder Name");
+	info->title = _("Bookmark");
+	info->label_top = _("Enter Folder Name");
 	info->text1 = g_strdup(settings.searchText);
-	info->label1 = N_("Folder: ");
+	info->label1 = _("Folder: ");
 	info->ok = TRUE;
 	info->cancel = TRUE;
 
