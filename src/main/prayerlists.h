9532f1088fbd8e84405c869be851d6c88bdacdde
Terry Biggs <tbiggs@users.sf.net>
2008-03-27 16:35:59 +0000

    add prayerlists.cc and prayerlists.h
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/branches/development/gnomesword@2099 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ src/main/prayerlists.h
@@ -0,0 +1,41 @@
+/*
+ * GnomeSword Bible Study Tool
+ * prayerlists.h -  code to create several different prayer lists
+ *
+ * Copyright (C) 2008 GnomeSword Developer Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+ 
+ 
+#ifndef _PRAYERLISTS_H
+#define _PRAYERLISTS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int main_prayerlist_basic_create(void); 
+int main_prayerlist_not_so_basic_create(void);
+int main_prayerlist_wild_create(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PRAYERLISTS_H */
+
+ 
