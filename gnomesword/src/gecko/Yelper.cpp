d2c74305718479a208175c79d59d3f34b25bd10d
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2008-05-22 20:50:39 +0000

    fixed to build and run in fedora 9
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/branches/tee@2135 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/src/gecko/Yelper.cpp
+++ gnomesword/src/gecko/Yelper.cpp
@@ -25,8 +25,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MOZILLA_INTERNAL_API
-#include <nsString.h>
+//#define MOZILLA_INTERNAL_API
+
+#include <nsStringAPI.h>
 
 #include <gtkmozembed.h>
 #include <gtkmozembed_internal.h>
@@ -162,7 +163,7 @@ void Yelper::JumpToAnchor(const char *anchor)
 	nsAutoString aAnchorName;
 	nsresult rv, result;
 	
-	aAnchorName.AssignWithConversion (anchor);
+	aAnchorName.Assign(NS_ConvertUTF8toUTF16(anchor));
 	
 	nsCOMPtr<nsIDocShell> docShell (do_GetInterface (mWebBrowser, &rv));
 
@@ -170,7 +171,7 @@ void Yelper::JumpToAnchor(const char *anchor)
 		return;
 	}
 
-	/* get nsIPresShell */
+	// * get nsIPresShell *
 
 	nsCOMPtr<nsIPresShell> presShell;
 	result = docShell->GetPresShell(getter_AddRefs(presShell));
@@ -178,6 +179,7 @@ void Yelper::JumpToAnchor(const char *anchor)
 		return;
 		
 	presShell->GoToAnchor(aAnchorName, 1);
+	
 }
 void
 Yelper::SetFindProperties (const char *aSearchString,
@@ -207,7 +209,7 @@ Yelper::Find (const char *aSearchString)
 #ifdef HAVE_GECKO_1_9
 	rv = mFinder->Find (NS_ConvertUTF8toUTF16 (aSearchString),
 			    PR_FALSE ,//  links only? *
-			    mHasFocus,
+			    //mHasFocus,
 			    &found);
 #else
 	rv = mFinder->Find (NS_ConvertUTF8toUTF16 (aSearchString),
@@ -297,7 +299,7 @@ Yelper::SetSelectionAttention (PRBool aAttention)
  
 gchar *Yelper::GetAttribute (nsIDOMNode *node, gchar *attribute)
 {
-     	nsresult result;
+ /*    	nsresult result;
 
 	nsCOMPtr<nsIDOMNamedNodeMap> attributes;
 	result = node->GetAttributes(getter_AddRefs(attributes));
@@ -316,7 +318,7 @@ gchar *Yelper::GetAttribute (nsIDOMNode *node, gchar *attribute)
 	result = attrNode->GetNodeValue(nodeValue);
 	if (!NS_SUCCEEDED(result))  return NULL;
 
-	return ToNewCString(nodeValue);
+	return ToNewCString(nodeValue);*/
 }
 
 gint
@@ -443,12 +445,13 @@ Yelper::ProcessMouseUpEvent (void* aEvent)
 	if (!event) return 0;
 		
 #ifdef DEBUG
-	nsAutoString aType;
+/*	nsAutoString aType;
 	domEvent->GetType(aType);
 	gchar mybuf[80];
-	aType.ToCString( mybuf, 79);
-	GS_message(("domEvent->GetType: %s",mybuf));
+	aType.nsCString(mybuf);
+	GS_message(("domEvent->GetType: %s",mybuf));*/
 #endif
+	
 	
 	PRUint16 button = 2;
 	event->GetButton (&button);
@@ -472,11 +475,11 @@ Yelper::ProcessMouseEvent (void* aEvent)
 	
 	
 #ifndef DEBUG
-	nsAutoString aType;	
+/*	nsAutoString aType;	
 	domEvent->GetType(aType);
 	gchar mybuf[80];
 	aType.ToCString( mybuf, 79);
-	GS_message(("domEvent->GetType: %s",mybuf));
+	GS_message(("domEvent->GetType: %s",mybuf));*/
 #endif	
 	
 	PRUint16 button = 2;
