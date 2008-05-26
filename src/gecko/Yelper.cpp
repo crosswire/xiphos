4d8a40a33112067529e51b327988b85381f17d13
charcoal <charcoal@da23c209-522a-4b7c-92e9-24af42be4764>
2008-05-26 22:51:35 +0000

    delete excess declaration
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/branches/development/gnomesword@2143 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gecko/Yelper.cpp
+++ src/gecko/Yelper.cpp
@@ -331,7 +331,6 @@ gint
 Yelper::ProcessMouseOver (void* aEvent, int pane, 
 			  gboolean is_dialog, DIALOG_DATA * dialog)
 {
-	extern gboolean in_url;
 	nsresult result;
 	extern gboolean in_url;
 	PRBool aShiftKey;
