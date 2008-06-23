e1165eb33e36001266dc142f25702ead0ce07869
Dominique Corbex <dominique.corbex@gmail.com>
2008-06-23 20:47:45 +0000

    dict/book navbar:Switch to Gtk stock icons (matching navbar_versekey.glade)
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/branches/development@2151 da23c209-522a-4b7c-92e9-24af42be4764

--- gnomesword/src/gnome2/navbar_book.c
+++ gnomesword/src/gnome2/navbar_book.c
@@ -331,8 +331,7 @@ GtkWidget *gui_navbar_book_new(void)
 	gtk_button_set_focus_on_click(GTK_BUTTON(navbar_book.button_left),
 				      FALSE);
 
-	image1 = gtk_image_new_from_icon_name("stock_left",
-					      GTK_ICON_SIZE_BUTTON);
+	image1 = gtk_image_new_from_stock(GTK_STOCK_GO_BACK,GTK_ICON_SIZE_BUTTON);
 	gtk_widget_show(image1);
 	gtk_container_add(GTK_CONTAINER(navbar_book.button_left), image1);
 
@@ -347,8 +346,7 @@ GtkWidget *gui_navbar_book_new(void)
 	gtk_button_set_focus_on_click(GTK_BUTTON(navbar_book.button_up),
 				      FALSE);
 
-	image1 = gtk_image_new_from_icon_name("stock_up",
-					      GTK_ICON_SIZE_BUTTON);
+	image1 = gtk_image_new_from_stock(GTK_STOCK_GO_UP,GTK_ICON_SIZE_BUTTON);
 	gtk_widget_show(image1);
 	gtk_container_add(GTK_CONTAINER(navbar_book.button_up), image1);
 
@@ -363,8 +361,7 @@ GtkWidget *gui_navbar_book_new(void)
 	gtk_button_set_focus_on_click(GTK_BUTTON(navbar_book.button_down),
 				      FALSE);
 
-	image2 = gtk_image_new_from_icon_name("stock_down",
-					      GTK_ICON_SIZE_BUTTON);
+	image2 = gtk_image_new_from_stock(GTK_STOCK_GO_DOWN,GTK_ICON_SIZE_BUTTON);
 	gtk_widget_show(image2);
 	gtk_container_add(GTK_CONTAINER(navbar_book.button_down), image2);
 
@@ -381,8 +378,8 @@ GtkWidget *gui_navbar_book_new(void)
 	gtk_button_set_focus_on_click(GTK_BUTTON(navbar_book.button_right),
 				      FALSE);
 
-	image1 = gtk_image_new_from_icon_name("stock_right",
-					      GTK_ICON_SIZE_BUTTON);
+	image1 = gtk_image_new_from_stock(GTK_STOCK_GO_FORWARD,
+									  GTK_ICON_SIZE_BUTTON);
 	gtk_widget_show(image1);
 	gtk_container_add(GTK_CONTAINER(navbar_book.button_right), image1);
 
