b06097194237a646a42c88d62f411b3d0f93b3d6
Dominique Corbex <dominique.corbex@gmail.com>
2009-01-28 20:25:26 +0000

    closes #2539788: Old address of FSF was used in all headers
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@2648 da23c209-522a-4b7c-92e9-24af42be4764

--- win32/nsis/svn2utf16.py
+++ win32/nsis/svn2utf16.py
@@ -17,7 +17,7 @@ GNU Library General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 """
  
 # Unicode NSIS (Nullsoft Scriptable Install System) expext its input text files
