c7ed7b6ae3ee7a44186acb8ac288f61c675e3006
Dominique Corbex <dominique.corbex@gmail.com>
2009-01-28 20:25:26 +0000

    closes #2539788: Old address of FSF was used in all headers
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@2648 da23c209-522a-4b7c-92e9-24af42be4764

--- src/backend/gs_stringmgr.cpp
+++ src/backend/gs_stringmgr.cpp
@@ -6,14 +6,21 @@
  *  gs_stringmgr was mostly inspired by BTSringMgr from BibleTime
  *  Credits to the BibleTime Team (http://www.bibletime.info)
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation version 2.
+ * Copyright (C) 2000-2009 Xiphos Developer Team
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, U.
  */
 
 #include "gs_stringmgr.h"
--- src/gecko/Yelper.cpp
+++ src/gecko/Yelper.cpp
@@ -14,7 +14,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  *  $Id: Yelper.cpp,v 1.7 2006/09/17 17:24:32 chpe Exp $
  */
