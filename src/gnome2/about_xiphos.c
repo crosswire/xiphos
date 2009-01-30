6d2ddaa8f0f448d8524d23bf657cbbdd9fceea8d
Karl Kleinpaste <karl@kleinpaste.org>
2009-01-30 03:40:46 +0000

    xiphos.sf.net is functional: replace gnomesword.sf.net with it
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@2652 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome2/about_xiphos.c
+++ src/gnome2/about_xiphos.c
@@ -127,7 +127,7 @@ gui_create_about_xiphos(void)
 	    (GTK_ABOUT_DIALOG (about1),
 	     _("(formerly known as GnomeSword)\nWe would like to thank Troy Griffitts and all the other folks who have given us The SWORD Project."));
 	gtk_about_dialog_set_website
-	    (GTK_ABOUT_DIALOG (about1), "http://gnomesword.sourceforge.net/");
+	    (GTK_ABOUT_DIALOG (about1), "http://xiphos.sourceforge.net/");
 	gtk_about_dialog_set_authors
 	    (GTK_ABOUT_DIALOG (about1), authors);
 	gtk_about_dialog_set_documenters
