5fd142e61eedbc5b902f4ea299b44911ece35a3e
homas Abthorpe <thomas@goodking.ca>
2009-02-19 13:44:55 +0000

    - Add recent translation contributors
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@2804 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome2/about_xiphos.c
+++ src/gnome2/about_xiphos.c
@@ -57,7 +57,7 @@ static const gchar *documenters[] = {
     NULL
 };
 
-static gchar *translators = "Jan Paul Schmidt\nJorge Chacon\nDominique Corbex\nZdenko Podobn\nPavlo Bohmat\nMartin Zibricky\nAri Constancio\nGertjan Francke\nEicke Godehardt";
+static gchar *translators = "Jan Paul Schmidt\nJorge Chacon\nDominique Corbex\nZdenko Podobn\nPavlo Bohmat\nMartin Zibricky\nAri Constancio\nGertjan Francke\nEicke Godehardt\nAndy Piper\nThomas Abthorpe\nEeli Kaikkonen";
 
 /******************************************************************************
  * Name
