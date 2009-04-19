9daf2e25cbc8bbce51e96ce298b7c821fce80ffa
Barry Drake <bdrake@crosswire.org>
2009-04-19 13:13:57 +0000

    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@2995 da23c209-522a-4b7c-92e9-24af42be4764

--- autogen.sh
+++ autogen.sh
@@ -19,7 +19,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
-# cr/lf changed to lf only
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
