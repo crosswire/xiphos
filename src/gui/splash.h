85ff608523ed7f30dc064e067a585977a393b78a
mwtalbert <ransom1982@gmail.com>
2009-05-05 17:22:25 +0000

    refactored splash; made icons light up in correct order; reversed icon order and light up order for rtl locales
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3046 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gui/splash.h
+++ src/gui/splash.h
@@ -27,11 +27,7 @@ GtkType l##_get_type(void)\
 }
 
 void gui_splash_init(void);
-void gui_splash_step0(void);
-void gui_splash_step1(void);
-void gui_splash_step2(void);
-void gui_splash_step3(void);
-void gui_splash_step4(void);
 void gui_splash_done(void);
+void gui_splash_step(gchar *text, gdouble progress, gint step);
 
 #endif
