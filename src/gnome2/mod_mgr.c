92c2779e18ddf67cd486a8be4988070b15587223
Karl Kleinpaste <karl@kleinpaste.org>
2009-05-25 11:08:40 +0000

    keep new non-anon ftp backward-compatible to 1.5.11
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3127 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome2/mod_mgr.c
+++ src/gnome2/mod_mgr.c
@@ -2503,14 +2503,14 @@ on_button7_clicked(GtkButton * button,
 	dialog->label2 = _("Type:");
 	dialog->label3 = _("Host:");
 	dialog->label4 = _("Directory:");
+#ifdef SWORD_MULTIVERSE
 	dialog->label5 = _("User (optional):");
 	dialog->label6 = _("Password (optional):");
+#endif
 	dialog->text1 = g_strdup("CrossWire");
 	dialog->text2 = g_strdup("FTP");
 	dialog->text3 = g_strdup("ftp.crosswire.org");
 	dialog->text4 = g_strdup("/pub/sword/raw");
-	dialog->text5 = g_strdup("");
-	dialog->text6 = g_strdup("");
 	dialog->cancel = TRUE;
 	dialog->ok = TRUE;
 
