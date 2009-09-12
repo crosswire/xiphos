c6cc9ff5fce5cc5823b3052186004a4413bb79c9
Karl Kleinpaste <karl@kleinpaste.org>
2009-09-12 19:52:12 +0000

    fix generation of locale_set.c by replicating 2-char names into 5-char names, all with .utf-8 suffix
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3372 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ src/gnome2/make-locale-set.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# convert LINGUAS file into a C source file containing full locale names.
+# names in LINGUAS are either 5-char full "language_LOCALE" names or
+# 2-char "language-only" names.  convert the latter.
+#
+echo 'char *locale_set[] = { '
+sed -e 's/en_GB/& en_US/' < ../../po/LINGUAS |
+tr ' ' '\n' |
+while read l ; do
+    case "$l" in
+	*_*)
+	    ;;
+	*)  u="`echo \"$l\" | tr a-z A-Z`"
+	    l="$l"_"$u"
+	    ;;
+    esac
+    echo '	"'"$l.UTF-8"'",'
+done
+echo '        (char*)0'
+echo '};'
