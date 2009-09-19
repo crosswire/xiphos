aa65f6d8a212ba49f04fe7c690078dc78d8e0027
matysek03 <matysek03@da23c209-522a-4b7c-92e9-24af42be4764>
2009-09-19 23:12:43 +0000

    waf: add creating help on win32 ( create CHM help files )
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3388 da23c209-522a-4b7c-92e9-24af42be4764

--- waffles/documentation.py
+++ waffles/documentation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Inspiration from gnome.py from waf
-
 "Create help files"
 
-import os, re
-import TaskGen, Utils, Runner, Task, Build, Options, Logs
+import os, re, shutil
+import Configure, TaskGen, Utils, Runner, Task, Build, Options, Logs
 import cc
 from Logs import error
 from TaskGen import taskgen, before, after, feature
@@ -14,104 +12,187 @@ from TaskGen import taskgen, before, after, feature
 
 class xml_doc_taskgen(TaskGen.task_gen):
     def __init__(self, *k, **kw):
-            TaskGen.task_gen.__init__(self, *k, **kw)
+        TaskGen.task_gen.__init__(self, *k, **kw)
+
 
 @feature('xml_doc')
 def init_xml_doc(self):
+    self.env['XML2PO_FLAGS'] = getattr(self, 'flags', ['-e', '-p'])
+
     if self.env['IS_WIN32']:
         self.default_install_path = '${PREFIX}/share/help'
+        self.env['HTMLHELP_XSL'] = os.path.normpath(os.path.join(
+                self.env['ROOTDIR'], 'share/docbook/htmlhelp/htmlhelp.xsl'))
     else:
         self.default_install_path = '${PREFIX}/share/gnome/help/xiphos'
+
 
 @feature('xml_doc')
 @after('init_xml_doc')
 def apply_xml_doc(self):
-    lst = self.to_list(self.doc_linguas)
-    bld = self.bld
-    for x in lst:
-        # xml2po - localized help
-        tsk = self.create_task('xml2po')
-        node = self.path.find_resource(os.path.join(x, x+'.po'))
-        src = self.path.find_resource(os.path.join('C', self.doc_module+'.xml'))
-        out = self.path.find_or_declare(os.path.join(x, self.doc_module+'.xml'))
-        tsk.set_inputs([node, src])
-        tsk.set_outputs(out)
 
-        # FIXME - implement making CHM help
+    def replace_backslash(node):
+        return node.abspath().replace('\\', '/')
+
+    def create_xml2po_task(lang):
+        node = self.path.find_resource(lang + os.sep + lang+'.po')
+        src = self.path.find_resource('C' + os.sep + self.doc_module+'.xml')
+        out = self.path.find_or_declare(lang + os.sep + self.doc_module+'.xml')
+
         if self.env['IS_WIN32']:
-            pass
+            # FIXME - xml2po on win32 fails with flags containing backslashes '\'
+            # failing example:  python xml2po -e -p fr\fr.po C\xiphos.xml
+            node_str = replace_backslash(node)
+            src_str = replace_backslash(src)
+
+            # it is necessary pas to xml2po only paths with '/'
+            Task.simple_task_type('xml2po',
+                    '${XML2PO} -e -p %s %s > ${TGT}' % (node_str, src_str),
+                    color='BLUE')
+
+        tsk = self.create_task('xml2po')
+        tsk.set_inputs([node, src])
+        # waf will control output file if it exists, when task is done
+        tsk.set_outputs(out)
+        return tsk, out
+
+    def create_xsltproc_task(lang, src, after=[]):
+        out = self.path.find_or_declare(lang + os.sep + 'htmlhelp.hhp')
+        tsk = self.create_task('xsltproc')
+        tsk.set_inputs(src)
+        tsk.set_outputs(out)
+        if after:
+            tsk.run_after.append(after)
+        return tsk, out
+
+    def create_htmlhelpc_task(lang, src, after=[]):
+        out = self.path.find_or_declare(lang + os.sep + 'htmlhelp.chm')
+        tsk = self.create_task('htmlhelpc')
+        tsk.set_inputs(src)
+        tsk.set_outputs(out)
+        tsk.run_after.append(after)
+        return tsk, out
+
+    def copy_figures(lang, figures, out):
+        fig1 = self.path.find_resource(os.path.join(lang, figures[0]))
+        srcdir = os.path.dirname(os.path.dirname(fig1.abspath(self.env)))
+        destdir = os.path.dirname(out.abspath(self.env))
+        for fig in figures:
+            shutil.copyfile(os.path.join(srcdir, fig), os.path.join(destdir, fig))
+
+
+    figures = self.to_list(self.doc_figures)
+    languages = self.to_list(self.doc_linguas)
+    bld = self.bld
+    # C help - doesn't need xml2po phase
+    if self.env['IS_WIN32']:
+        C_src = self.path.find_resource('C' + os.sep + self.doc_module+'.xml')
+        C_tsk, C_out = create_xsltproc_task('C', C_src)
+        copy_figures('C', figures, C_out)
+        create_htmlhelpc_task('C', C_out, C_tsk)
+
+    # translated help
+    for lang in languages:
+        # xml2po - localized help
+        tsk, out = create_xml2po_task(lang)
+
+        if self.env['IS_WIN32']:
             # xsltproc - create html files
+            tsk2, out2 = create_xsltproc_task(lang, out, tsk)
+            # copy figures - hhc needs figures in the build dir
+            copy_figures(lang, figures, out2)
+            # CHM help - windows help format
+            tsk3, out3 = create_htmlhelpc_task(lang, out2, tsk2)
 
-            #tsk2 = self.create_task('xsltproc')
-            #out2 = self.path.find_or_declare('%s/%s-%s.omf' % (x, self.doc_module, x))
-            #tsk2.set_outputs(out2)
-            #node = self.path.find_resource(self.doc_module+".omf.in")
-            #tsk2.inputs = [node, out]
-
-            # htmlhelp - MS help in CHM format
-
-            #tsk2.run_after.append(tsk)
-            #tsk3.run_after.append(tsk2)
-
-        if bld.is_install:
-            path = os.path.join(self.install_path, x)
-            for y in self.to_list(self.doc_figures):
-                try:
-                    os.stat(os.path.join(self.path.abspath(), x, y))
-                    bld.install_as(path + '/' + y, self.path.abspath() + '/' + x + '/' + y)
-                except:
-                    bld.install_as(path + '/' + y, self.path.abspath() + '/C/' + y)
-            bld.install_as(path + '/%s.xml' % self.doc_module, out.abspath(self.env))
+        #if bld.is_install:
+            #path = self.install_path + '/' + x
+            #for y in self.to_list(self.doc_figures):
+                #try:
+                    #os.stat(self.path.abspath()+'/'+ x +'/'+ y)
+                    #bld.install_as(path + '/' + y, self.path.abspath() + '/' + x + '/' + y)
+                #except:
+                    #bld.install_as(path + '/' + y, self.path.abspath() + '/C/' + y)
+            #bld.install_as(path + '/%s.xml' % self.doc_module, out.abspath(self.env))
 
     # install help for 'C'
-    if bld.is_install:
-        path = os.path.join(self.install_path, 'C')
-        lst = self.to_list(self.doc_includes) + self.to_list(self.doc_figures)
-        for y in lst:
-            bld.install_as(path + '/' + y, self.path.abspath() + '/' + 'C' + '/' + y)
+    #if bld.is_install:
+        #path = os.path.join(self.install_path, 'C')
+        #path = self.install_path + '/' + 'C'
+        #lst = self.to_list(self.doc_includes) + self.to_list(self.doc_figures)
+        #for y in lst:
+            #bld.install_as(path + '/' + y, self.path.abspath() + '/' + 'C' + '/' + y)
         # install 'xiphos.xml' and 'legal.xml'
-        bld.install_as(path + '/%s.xml' % self.doc_module,
-                self.path.abspath() + '/C/%s.xml' % self.doc_module)
-        bld.install_as(path + '/%s.xml' % self.doc_entities,
-                self.path.abspath() + '/C/%s' % self.doc_entities)
+        #bld.install_as(path + '/%s.xml' % self.doc_module,
+                #self.path.abspath() + '/C/%s.xml' % self.doc_module)
+        #bld.install_as(path + '/%s.xml' % self.doc_entities,
+                #self.path.abspath() + '/C/%s' % self.doc_entities)
 
 
+def exec_xsltproc(self):
+    '''
+    xsltproc save files to cwd (current working directory).
+    CWD in this case is folder where is translated documentation.
+    '''
+    env = self.env
+    input = self.inputs[0].abspath(env)
+    cwd = os.path.dirname(self.outputs[0].abspath(env))
 
-Task.simple_task_type('xml2po', '${XML2PO} ${XML2PO_FLAGS} ${SRC} > ${TGT}', color='BLUE')
+    # cmd='xsltproc htmlhelp.xsl xiphos.xml'
+    lst = []
+    lst.append(env['XSLTPROC'])
+    lst.append(env['HTMLHELP_XSL'])
+    lst.append(input)
 
-# how do you expect someone to understand this?!
-#xslt_magic = """${XSLTPROC2PO} -o ${TGT[0].abspath(env)} \
-#--stringparam db2omf.basename ${APPNAME} \
-#--stringparam db2omf.format docbook \
-#--stringparam db2omf.lang C \
-#--stringparam db2omf.dtd '-//OASIS//DTD DocBook XML V4.3//EN' \
-#--stringparam db2omf.omf_dir ${PREFIX}/share/omf \
-#--stringparam db2omf.help_dir ${PREFIX}/share/gnome/help \
-#--stringparam db2omf.omf_in ${SRC[0].abspath(env)} \
-#--stringparam db2omf.scrollkeeper_cl ${SCROLLKEEPER_DATADIR}/Templates/C/scrollkeeper_cl.xml \
-#${DB2OMF} ${SRC[1].abspath(env)}"""
+    lst = [lst]
+    ret = self.exec_command(*lst, cwd=cwd)
 
-#Task.simple_task_type('xsltproc2po', xslt_magic, color='BLUE')
+    return ret
+
+def exec_htmlhelpc(self):
+    '''
+    hhc also saves files to cwd (current working directory).
+    '''
+    env = self.env
+    input = self.inputs[0].abspath(env)
+    cwd = os.path.dirname(input)
+
+    lst = []
+    lst.append(env['HTMLHELPC'])
+    lst.append(input)
+
+    lst = [lst]
+    ret = self.exec_command(*lst, cwd=cwd)
+
+    return ret
+
+
+Task.simple_task_type('xml2po', '${XML2PO} -o ${TGT} ${XML2PO_FLAGS} ${SRC}', color='BLUE', shell=False)
+Task.task_type_from_func('xsltproc', vars=['XSLTPROC', 'HTMLHELP_XSL'], color='BLUE', func=exec_xsltproc)
+Task.task_type_from_func('htmlhelpc', vars=['HTMLHELPC'], color='BLUE', func=exec_htmlhelpc)
+
 
 def detect(conf):
-    #conf.check_tool('gnu_dirs glib2 dbus')
-    #sgml2man = conf.find_program('docbook2man', var='SGML2MAN')
-
-    #def getstr(varname):
-            #return getattr(Options.options, varname, '')
-
-    # addefine also sets the variable to the env
-    #conf.define('GNOMELOCALEDIR', os.path.join(conf.env['DATADIR'], 'locale'))
-
-    # FIXME - add python to find_program - on win32 'xml2po' is not executable
-    #         find_file
+    # xml2po
     xml2po = conf.find_program('xml2po', var='XML2PO')
+    if not xml2po:
+        # xml2po: on win32 'xml2po' is not executable thus this hook
+        if conf.env['IS_WIN32']:
+            python = conf.find_program('python', var='PYTHON')
+            if not python:
+                conf.fatal('The program python (required by xml2po) could not be found')
 
-    # flags ensure that translated XML will be in one file
-    conf.env['XML2PO_FLAGS'] = '-e -p'
+            xml2podir = Configure.find_file('xml2po', os.environ['PATH'].split(os.pathsep))
+            if not xml2podir:
+                conf.fatal('The program xml2po is mandatory!')
+
+            conf.env['XML2PO'] = Utils.to_list(conf.env['PYTHON']) + [xml2podir + os.sep + 'xml2po']
+            conf.check_message('xml2po', '', True, ' '.join(conf.env['XML2PO']))
+
+        else:
+            conf.fatal('The program xml2po is mandatory!')
+
 
     if conf.env['IS_WIN32']:
-        # FIXME - any xsltproc flags necessary?
         xsltproc = conf.find_program('xsltproc', var='XSLTPROC')
         htmlhelpc = conf.find_program('hhc', var='HTMLHELPC')
         
