35ad463fa64e444df716b7a0612537424d591481
ransom1982 <ransom1982@da23c209-522a-4b7c-92e9-24af42be4764>
2009-09-21 14:53:37 +0000

    generating help is now done with waf
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3390 da23c209-522a-4b7c-92e9-24af42be4764

--- win32/makeDistro.sh
+++ win32/makeDistro.sh
@@ -30,26 +30,6 @@ cd ..
 ./waf
 ./waf install
 
-# convert English help manual to chm
-cd help/C
-xsltproc /share/docbook/htmlhelp/htmlhelp.xsl xiphos.xml
-hhc htmlhelp.hhp
-cp htmlhelp.chm ../../win32/binaries/Xiphos/share/help/C/xiphos.chm
-
-# merge French po into xml
-for file in *.xml
-do
-    xml2po -e -p ../fr/fr.po -o ../fr/$file $file
-done
-
-# convert French help manual to chm
-cd ../fr
-xsltproc /share/docbook/htmlhelp/htmlhelp.xsl xiphos.xml
-hhc htmlhelp.hhp
-cp htmlhelp.chm ../../win32/binaries/Xiphos/share/help/fr/xiphos_fr.chm
-
-cd ../../
-
 # copy sword utils and libs
 outdir=win32/binaries/Xiphos/bin/
 origdir=/usr/local/bin/
