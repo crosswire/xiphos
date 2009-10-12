144cb4ffaddef16353d8654607e704004bf3fb80
mwtalbert <ransom1982@gmail.com>
2009-10-12 03:59:00 +0000

    compiles
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3427 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ src/gui/ipc.h
@@ -0,0 +1,52 @@
+#ifndef __XIPHOS_IPC_H__
+#define __XIPHOS_IPC_H__
+
+#include <glib.h>
+#include <dbus/dbus-glib.h>
+
+G_BEGIN_DECLS
+
+#define IPC_TYPE_OBJECT             (ipc_object_get_type())
+#define IPC_OBJECT(object) \
+	(G_TYPE_CHECK_INSTANCE_CAST ((object), \
+	 IPC_TYPE_OBJECT, IpcObject))
+#define IPC_OBJECT_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_CAST ((klass), \
+	 IPC_TYPE_OBJECT, IpcObjectClass))
+#define IPC_IS_OBJECT(klass) \
+	(G_TYPE_CHECK_CLASS_TYPE ((klass), \
+	 IPC_TYPE_OBJECT))
+#define IPC_IS_OBJECT_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_TYPE ((klass), \
+	 IPC_TYPE_OBJECT))
+#define IPC_OBJECT_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS ((obj), \
+	 IPC_TYPE_OBJECT, IpcObjectClass))
+
+typedef enum {
+	SEARCH_PERFORMED,
+	LAST_SIGNAL
+} IpcSignalNumber;
+
+typedef struct {
+	GObject parent;
+	GList *references;
+} IpcObject;
+
+typedef struct {
+	GObjectClass parent;
+	guint signals[LAST_SIGNAL];
+} IpcObjectClass;
+
+GType ipc_object_get_type(void);
+
+G_DEFINE_TYPE(IpcObject, ipc_object, G_TYPE_OBJECT)
+
+gboolean ipc_object_search_performed (IpcObject* obj, 
+				      const gchar* reference,
+				      GError** error);
+IpcObject* ipc_init_dbus_connection(IpcObject* obj);
+
+G_END_DECLS
+
+#endif /* __XIPHOS_IPC_H__ */
