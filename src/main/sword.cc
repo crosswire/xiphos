544cbfacea1b1ec1bf4d14bc4965732ff384308b
ransom1982 <ransom1982@da23c209-522a-4b7c-92e9-24af42be4764>
2009-12-02 13:51:35 +0000

    cleanup
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3494 da23c209-522a-4b7c-92e9-24af42be4764

--- src/main/sword.cc
+++ src/main/sword.cc
@@ -32,7 +32,6 @@
 #include <swmodule.h>
 #include <stringmgr.h>
 #include <localemgr.h>
-#include <filemgr.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -668,7 +667,6 @@ char *set_sword_locale(const char *sys_locale)
 void main_init_backend(void)
 {
 	StringMgr::setSystemStringMgr( new GS_StringMgr() );
-	FileMgr::getSystemFileMgr()->maxFiles = 25000;
 	
 	const char *lang = getenv("LANG");
 	if (!lang) lang = "C";
