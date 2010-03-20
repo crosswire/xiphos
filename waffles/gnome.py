cd091a20088007744313538fe1ba7e8725ecc1da
dmitrijledkov <dmitrijledkov@da23c209-522a-4b7c-92e9-24af42be4764>
2010-03-20 23:35:02 +0000

    Fixed omf file location
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3747 da23c209-522a-4b7c-92e9-24af42be4764

--- waffles/gnome.py
+++ waffles/gnome.py
@@ -89,7 +89,7 @@ def apply_gnome_doc(self):
 
 		if bld.is_install:
 			path = self.install_path + '/gnome/help/%s/%s' % (self.doc_module, x)
-			bld.install_files(self.install_path + '/omf', out2, env=self.env)
+			bld.install_files(self.install_path + '/omf/%s/' % (self.doc_module), out2, env=self.env)
 			for y in self.to_list(self.doc_figures):
 				try:
 					os.stat(self.path.abspath() + '/' + x + '/' + y)
