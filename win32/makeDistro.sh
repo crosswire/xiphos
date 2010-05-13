28fc006fdba2fd5399ae175d61869de1751525fe
charcoal <charcoal@da23c209-522a-4b7c-92e9-24af42be4764>
2010-05-13 23:25:26 +0000

    remove duplicated .exe
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@3861 da23c209-522a-4b7c-92e9-24af42be4764

--- win32/makeDistro.sh
+++ win32/makeDistro.sh
@@ -34,7 +34,7 @@ cd ..
 outdir=win32/binaries/Xiphos/bin/
 origdir=/usr/local/bin/
 
-for f in libsword-1-6-0-r2494.dll imp2ld.exe addld.exe mod2zmod.exe imp2gbs.exe xml2gbs.exe installmgr.exe imp2vs.exe vpl2mod.exe mkfastmod.exe mod2vpl.exe vs2osisreftxt.exe vs2osisreftxt.exe tei2mod.exe osis2mod.exe mod2osis.exe mod2imp.exe diatheke.exe uconv.exe icuin40.dll icuuc40.dll
+for f in libsword-1-6-0-r2494.dll imp2ld.exe addld.exe mod2zmod.exe imp2gbs.exe xml2gbs.exe installmgr.exe imp2vs.exe vpl2mod.exe mkfastmod.exe mod2vpl.exe vs2osisreftxt.exe tei2mod.exe osis2mod.exe mod2osis.exe mod2imp.exe diatheke.exe uconv.exe icuin40.dll icuuc40.dll
 do
     cp $origdir$f $outdir$f
     strip $outdir$f
