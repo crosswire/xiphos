1f87a3b833dc29bdb417e1a440289f93bd48e2f8
homas Abthorpe <thomas@goodking.ca>
2010-10-21 14:35:10 +0000

    - Remove hardwired path to pkg-config
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@4120 da23c209-522a-4b7c-92e9-24af42be4764

--- waffles/gnome.py
+++ waffles/gnome.py
@@ -216,7 +216,7 @@ def detect(conf):
 	xsltproc2po = conf.find_program('xsltproc', var='XSLTPROC2PO')
 	conf.env['XML2POFLAGS'] = '-e -p'
 	conf.env['SCROLLKEEPER_DATADIR'] = Utils.cmd_output("scrollkeeper-config --pkgdatadir", silent=1).strip()
-	conf.env['DB2OMF'] = Utils.cmd_output("/usr/bin/pkg-config --variable db2omf gnome-doc-utils", silent=1).strip()
+	conf.env['DB2OMF'] = Utils.cmd_output("pkg-config --variable db2omf gnome-doc-utils", silent=1).strip()
 
 def set_options(opt):
 	opt.add_option('--want-rpath', type='int', default=1, dest='want_rpath', help='set rpath to 1 or 0 [Default 1]')
