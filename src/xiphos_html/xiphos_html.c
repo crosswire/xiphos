e1cab69b3fa39068cc7b7ed815b3b278ee09ce1b
Karl Kleinpaste <karl@kleinpaste.org>
2011-06-07 12:14:21 +0000

    maintaining compatibility with gecko
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/branches/webkit@4182 da23c209-522a-4b7c-92e9-24af42be4764

--- src/xiphos_html/xiphos_html.c
+++ src/xiphos_html/xiphos_html.c
@@ -5,16 +5,23 @@ XiphosHtml *xiphos_html_new(DIALOG_DATA * dialog, gboolean is_dialog, gint pane)
 	XiphosHtml *html;
 	html = XIPHOS_HTML(g_object_new(XIPHOS_TYPE_HTML, NULL));
 	XiphosHtmlPriv *priv = XIPHOS_HTML_GET_PRIVATE(html);
+
+#if !defined(USE_WEBKIT) && defined(USE_GTKMOZEMBED)
+	/* gecko, which has weird "structure privacy" needs */
+	gecko_html_set_parms(html, priv, dialog, is_dialog, pane);
+#else
+	/* webkit or gtkhtml */
 	priv->pane = pane;
 	priv->is_dialog = is_dialog;
 	priv->dialog = dialog;
 	
 
-#ifdef USE_GTKMOZEMBED
+# ifdef USE_GTKMOZEMBED
 	//make scrollbar appear on the left for RTL locales
 	GtkTextDirection dir = gtk_widget_get_direction(GTK_WIDGET (html));
 	if (dir == GTK_TEXT_DIR_RTL)
 	    priv->yelper->SetRTL();
-#endif
+# endif /* USE_GTKMOZEMBED */
+#endif /* USE_WEBKIT || !USE_GTKMOZEMBED */
 	return html;
 }
