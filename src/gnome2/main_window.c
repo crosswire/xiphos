006a8d458db1322504535168112b572c9ef1ad9b
charcoal <charcoal@da23c209-522a-4b7c-92e9-24af42be4764>
2011-10-20 14:05:00 +0000

    changes for standard new/close tab buttons, from Mark__T
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/branches/webkit@4312 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome2/main_window.c
+++ src/gnome2/main_window.c
@@ -835,7 +835,9 @@ void create_mainwindow(void)
 	widgets.button_new_tab = gtk_button_new();
 	//don't show button here in case !settings.browsing
 
-	tab_button_icon = pixmap_finder("new_tab_button.png");
+	tab_button_icon = gtk_image_new_from_icon_name("stock_new-tab", GTK_ICON_SIZE_SMALL_TOOLBAR);
+	// ...or perhaps gtk_image_new_from_stock(GTK_STOCK_ADD, ...)
+
 	gtk_widget_show(tab_button_icon);
 	gtk_container_add(GTK_CONTAINER(widgets.button_new_tab), tab_button_icon);
 	gtk_button_set_relief(GTK_BUTTON(widgets.button_new_tab), GTK_RELIEF_NONE);
