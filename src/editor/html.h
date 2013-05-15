cfde536bcafe42b2f4b61698f8c486d1e0c9fbc6
tbiggs <tbiggs@da23c209-522a-4b7c-92e9-24af42be4764>
2013-05-15 18:53:44 +0000

    oops forgot one
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@4456 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ src/editor/html.h
@@ -0,0 +1,49 @@
+#define html_head \
+"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"> \n \
+<html> \n \
+<head>  \n \
+<title>studypad</title> \n \
+<meta name=\"generator\" content=\"Studypad\" > \n \
+<meta name=\"author\" content=\"Terry Biggs\" > \n \
+<meta name=\"date\" content=\"2013-04-18T10:30:48-0400\" > \n \
+<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"> \n \
+<meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\"> \n \
+<meta http-equiv=\"content-style-type\" content=\"text/css\"> \n \
+<meta http-equiv=\"expires\" content=\"0\"> \n \
+<style content=\"text/css\"> \n \
+ a { \n \
+	text-decoration:none;  \n \
+ } \n \
+ span.M1 {  \n \
+ 	font-family:Droid Sans,sans-serif;  \n \
+ 	font-size: 14px;  \n \
+ 	font-weight: normal;  \n \
+ }  \n \
+ span.transChangeSupplied  {  \n \
+ 	font-style: italic; \n \
+ }  \n \
+ol.L1 { \n \
+	list-style-type: upper-roman; \n \
+	font-size: 1.3em; \n \
+	font-weight: bold; \n \
+	margin-left: -10px; \n \
+} \n \
+ol.L2 { \n \
+	list-style-type: upper-alpha; \n \
+	font-size: .75em; \n \
+	font-weight: normal; \n \
+	margin-left: -6px; \n \
+} \n \
+ol.L3 { \n \
+	list-style-type: decimal; \n \
+	font-weight: normal; \n \
+	margin-left: 5px; \n \
+} \n \
+ol.L4 { \n \
+	list-style-type: lower-alpha; \n \
+	font-weight: normal; \n \
+	margin-left: 10px; \n \
+} \n \
+</style> \n \
+</head> \n \
+<body contenteditable=\"true\"> "
