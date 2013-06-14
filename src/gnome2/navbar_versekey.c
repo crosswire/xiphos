5888e4d8602baa788b67ca00232106f281052655
Karl Kleinpaste <karl@kleinpaste.org>
2013-06-14 21:46:46 +0000

    avoid undeclared variable for gtk2
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@4470 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome2/navbar_versekey.c
+++ src/gnome2/navbar_versekey.c
@@ -942,7 +942,9 @@ GtkWidget *gui_navbar_versekey_new(void)
 #else
 	GladeXML *gxml;
 #endif
+#ifdef USE_GTK_3
 	GtkWidget * eventbox;
+#endif
 	
 	gchar *glade_file = gui_general_user_file("navbar_versekey" UI_SUFFIX, FALSE);
 	g_return_val_if_fail((glade_file != NULL), NULL);
