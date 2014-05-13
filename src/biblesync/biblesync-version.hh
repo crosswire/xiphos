7eb2f1bbc8a8211a02aae4b1ffdd50dfbe907ad3
charcoal <charcoal@da23c209-522a-4b7c-92e9-24af42be4764>
2014-05-13 15:34:36 +0000

    for now, keeping up with small changes from external biblesync tree
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@4596 da23c209-522a-4b7c-92e9-24af42be4764

---
+++ src/biblesync/biblesync-version.hh
@@ -0,0 +1,27 @@
+/******************************************************************************
+ *
+ * biblesync-version.h - version constants.
+ *
+ * BibleSync library
+ * Karl Kleinpaste, May 2014
+ *
+ * All files related to implementation of BibleSync, including program
+ * source, READMEs, manual pages, and related similar documents, are in
+ * the public domain.  As a matter of simple decency, your social
+ * obligations are to credit the source and to coordinate any changes you
+ * make back to the origin repository.  These obligations are non-
+ * binding for public domain software, but they are to be seriously
+ * handled nonetheless.
+ */
+
+#ifndef BIBLESYNC_VERSION_HH
+#define BIBLESYNC_VERSION_HH
+
+#define BIBLESYNC_VERSION_NUM 1000000000
+#define BIBLESYNC_VERSION_STR "1.0.0"
+#define BIBLESYNC_VERSION_MAJOR 1
+#define BIBLESYNC_VERSION_MINOR 0
+#define BIBLESYNC_VERSION_MICRO 0
+#define BIBLESYNC_VERSION_NANO 0
+
+#endif	// BIBLESYNC_VERSION_HH
