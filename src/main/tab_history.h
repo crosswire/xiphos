29435fc9e9e1c9e796b9d04989687f410d9e1d9e
Karl Kleinpaste <karl@kleinpaste.org>
2014-10-30 22:55:02 +0000

    add history -> verse list loader.
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@4650 da23c209-522a-4b7c-92e9-24af42be4764

--- src/main/tab_history.h
+++ src/main/tab_history.h
@@ -39,6 +39,7 @@ void on_menu_historyitem_activate(GtkMenuItem *menuitem,
 					  gpointer user_data);
 void main_update_tab_history_menu(gpointer data);
 void on_clear_activate(GtkMenuItem * menuitem, gpointer user_data);
+void on_history_to_verse_list_activate(GtkMenuItem * menuitem, gpointer user_data);
 GtkWidget *main_versekey_drop_down_new(gpointer data);
 void main_clear_tab_history(void);
 void main_add_tab_history_item(gpointer data);
