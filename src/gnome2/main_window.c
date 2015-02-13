bd81b7787301737d2412912c97c48b15d13f16fb
Christopher Bayliss <christopher.j.bayliss@gmail.com>
2015-02-13 21:35:24 +0000

    Add enable/disable statusbar option to GTK2 and older GTK3
    git-svn-id: http://svn.code.sf.net/p/gnomesword/code/trunk@4876 da23c209-522a-4b7c-92e9-24af42be4764

--- src/gnome2/main_window.c
+++ src/gnome2/main_window.c
@@ -1139,13 +1139,10 @@ void create_mainwindow(void)
 
 	widgets.appbar = gtk_statusbar_new();
 
-#ifdef HAVE_GTK_314
 	if (settings.statusbar) {
 		gtk_widget_show(widgets.appbar);
 	}
-#else
-	gtk_widget_show(widgets.appbar);
-#endif
+
 #ifndef USE_GTK_3
 	gtk_statusbar_set_has_resize_grip(GTK_STATUSBAR(widgets.appbar),
 					  TRUE);
