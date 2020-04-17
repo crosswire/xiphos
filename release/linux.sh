dbd58558b3951c10ab84620f8213bee8ebd64990
Gregory Hellings <greg.hellings@gmail.com>
2020-04-16 20:33:49 -0500

    Try to fix dep installs

--- release/linux.sh
+++ release/linux.sh
@@ -3,6 +3,8 @@ set -ex -o pipefail
 CMAKE_OPTS=
 if [ $(command -v apt-get) ]; then
 	apt-get update
+	export DEBIAN_FRONTEND=noninteractive
+	ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 	apt-get -y install appstream-util cmake g++ desktop-file-utils \
 		fp-utils gsettings-desktop-schemas-dev intltool itstool libdbus-glib-1-dev \
 		libenchant-dev libgail-3-dev libglade2-dev libgsf-1-dev libgtk-3-dev \
@@ -11,7 +13,7 @@ if [ $(command -v apt-get) ]; then
 		libsword-dev zip
 	CMAKE_OPTS="-DPACKAGE_RPM:BOOL=OFF"
 elif [ $(command -v dnf) ]; then
-	dnf install -y 'dnf-command(builddep)' make rpmbuild zip
+	dnf install -y 'dnf-command(builddep)' make rpm-build zip
 	dnf builddep -y xiphos
 	CMAKE_OPTS="-DPACKAGE_DEB:BOOL=OFF"
 fi
