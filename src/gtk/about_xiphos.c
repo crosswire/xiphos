ffd7b7cca62496723aa991b0565773eb88e4d7a3
L'Africain <fr.cyrille@tiberiade.be>
2020-05-18 16:11:03 +0200

    Two new entries for locale in the IBT repository (#1031)
    * Two new entries for locale in the IBT repository
    
    * New name in the list of translators
    
    Co-authored-by: lafricain79 <lafricain79@gmail.com>

--- src/gtk/about_xiphos.c
+++ src/gtk/about_xiphos.c
@@ -58,7 +58,7 @@ static const gchar *documenters[] = {
     NULL};
 
 static gchar *translators =
-    "Jan Paul Schmidt\nJorge Chacon\nDominique Corbex\nZdenko Podobn\nPavlo Bohmat\nMartin Zibricky\nAri Constancio\nGertjan Francke\nEicke Godehardt\nAndy Piper\nThomas Abthorpe\nEeli Kaikkonen";
+    "Jan Paul Schmidt\nJorge Chacon\nDominique Corbex\nCyrille Mattiuzzo\nZdenko Podobn\nPavlo Bohmat\nMartin Zibricky\nAri Constancio\nGertjan Francke\nEicke Godehardt\nAndy Piper\nThomas Abthorpe\nEeli Kaikkonen";
 
 /******************************************************************************
  * Name
