e6f9d074018e75d3aeb3939cf96e92c741ff1080
Karl Kleinpaste <karl@kleinpaste.org>
2024-08-15 14:35:35 -0400

    #1170 remove an unneeded data bit that was missed in the big commit yesteday.

--- src/main/settings.h
+++ src/main/settings.h
@@ -60,8 +60,7 @@ struct _settings
 	    *highlight_fg,						    /* special fg color for highlight */
 	    *highlight_bg,						    /* special bg color for highlight */
 	    *verse_num_font_size_str, *base_font_size_str,
-	    *special_locale,						    /* prefs dialog: pick abnormal locale */
-	    *display_columns_str;
+	    *special_locale;						    /* prefs dialog: pick abnormal locale */
 	/* --requires restart-- */
 
 	int
